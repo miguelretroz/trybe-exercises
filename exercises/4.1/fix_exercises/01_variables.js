a025560ad9e42c9f3f64b8de25350b0a7c68a98f
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-23 19:23:38 -0300

    Const error

--- exercises/4.1/fix_exercises/01_variables.js
+++ exercises/4.1/fix_exercises/01_variables.js
@@ -6,3 +6,10 @@ console.log(name, birthCity, birthYear);
 
 birthYear = 2030;
 console.log(birthYear);
+
+/*
+birthCity = "Itu";
+console.log(birthCity);
+--> Ocorre um erro pois não é possível alterar o
+valor de uma constante. <--
+*/
