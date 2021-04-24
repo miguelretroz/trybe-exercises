6b90f9b6384a773f6c07347a9b6b501d5c092fa0
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 19:52:14 -0300

    Checks if have even number

--- exercises/4.1/exercises/08_is_even.js
+++ exercises/4.1/exercises/08_is_even.js
@@ -1,3 +1,11 @@
 let firstNumber =  Math.ceil(Math.random() * 100);
 let secondNumber = Math.ceil(Math.random() * 100);
 let thridNumber = Math.ceil(Math.random() * 100);
+
+console.log(`The numbers chosen were -> ${firstNumber}, ${secondNumber}, ${thridNumber} !`);
+
+if ((firstNumber % 2) === 0 || (secondNumber % 2) === 0 || (thridNumber % 2) === 0) {
+    console.log("True!");
+} else {
+    console.log('False!')
+};
