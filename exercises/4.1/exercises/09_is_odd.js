28d3877f3e6d4ad7b5689ffdf4a6bec476fea45a
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 20:04:57 -0300

    Checks if have odd number

--- exercises/4.1/exercises/09_is_odd.js
+++ exercises/4.1/exercises/09_is_odd.js
@@ -4,3 +4,8 @@ let thridNumber = Math.ceil(Math.random() * 50);
 
 console.log('The numbers chosen were => ' + firstNumber + ", " + secondNumber + ', ' + thridNumber);
 
+if ((firstNumber % 2) !== 0 || (secondNumber % 2) !== 0 || (thridNumber % 2) !== 0) {
+    console.log("True!");
+} else {
+    console.log("False!");
+};
