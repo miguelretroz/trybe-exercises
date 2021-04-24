fd5704c1e013b85a32a53a4d1839756c43a23a22
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 06:43:18 -0300

    Ending angle verification

--- exercises/4.1/exercises/05_is_triangle.js
+++ exercises/4.1/exercises/05_is_triangle.js
@@ -1,5 +1,11 @@
 let angleA = 30;
-let angleB = 100;
-let angleC = 50;
+let angleB = 90;
+let angleC = 60;
 
 let angleTotal = angleA + angleB + angleC;
+
+if ((angleA > 0 || angleB > 0 || angleC > 0) && angleTotal === 180) {
+    console.log('True!');
+} else {
+    console.log('False!');
+};
