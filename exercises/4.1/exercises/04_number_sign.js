9b4b4a68b93818e580d229359d38af144d88e74b
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 06:21:46 -0300

    Ending the exercise

---
+++ exercises/4.1/exercises/04_number_sign.js
@@ -0,0 +1,9 @@
+let number = 8;
+
+if (number > 0) {
+    console.log('Number is positive!');
+} else if (number < 0) {
+    console.log('Number is negative!');
+} else {
+    console.log('Number is zero!');
+};
