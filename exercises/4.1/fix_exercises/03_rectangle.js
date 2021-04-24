0aa6f0dbf3a1a3d0087fa08b790b48f738259e86
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-23 22:00:57 -0300

    Rectangle perimeter

--- exercises/4.1/fix_exercises/03_rectangle.js
+++ exercises/4.1/fix_exercises/03_rectangle.js
@@ -2,4 +2,7 @@ let base = 5;
 let altura = 8;
 
 let area = base * altura;
-console.log(`Área do retangulo -> ${area}`);
+console.log(`Área do retângulo -> ${area}`);
+
+let perimetro = (base * 2) + (altura * 2);
+console.log(`Perímetro do retângulo -> ${perimetro}`);
