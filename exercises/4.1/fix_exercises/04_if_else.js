80716941b1a41983ba5313dc5330c3052b85a50a
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-23 22:55:39 -0300

    Add switch/case

--- exercises/4.1/fix_exercises/04_if_else.js
+++ exercises/4.1/fix_exercises/04_if_else.js
@@ -1,7 +1,7 @@
 let score = 59;
 
 if (score >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!");
+    console.log('Parabéns, você foi aprovada(o)!');
 } else if (score >= 60) {
     console.log('Você está na nossa lista de espera.');
 } else {
--- exercises/4.1/fix_exercises/05_switch_case.js
+++ exercises/4.1/fix_exercises/05_switch_case.js
@@ -1,4 +1,4 @@
-let score = 80;
+let score = 50;
 let status;
 
 if (score >= 80) {
@@ -7,4 +7,19 @@ if (score >= 80) {
     status = 'lista';
 } else {
     status = 'reprovada';
+}
+
+switch(status) {
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+    case "lista":
+        console.log('Você está na nossa lista de espera.');
+        break;
+    case 'reprovada':
+        console.log('Você foi reprovada(o).');
+        break;
+    default:
+        console.log('Não se aplica.');
+        break;
 }
