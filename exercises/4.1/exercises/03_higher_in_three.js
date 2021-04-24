b4501811357591726588acbec72d10fc9d8a95bf
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 06:05:40 -0300

    Finding higher number

--- exercises/4.1/exercises/03_higher_in_three.js
+++ exercises/4.1/exercises/03_higher_in_three.js
@@ -1,3 +1,15 @@
 let firstNumber = Math.ceil(Math.random() * 100);
 let secondNumber = Math.ceil(Math.random() * 100);
 let thirdNumber = Math.ceil(Math.random() * 100);
+
+let higher = 0;
+
+if (a > b) {
+    higher = a;
+} else {
+    higher = b;
+}
+
+if (c > higher) {
+    higher = c;
+}
