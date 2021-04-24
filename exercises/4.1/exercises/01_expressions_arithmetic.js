76a6906ece0ac25a24f4aef1da9ebec8da2b0cad
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 05:28:49 -0300

    Add expressions

--- exercises/4.1/exercises/01_expressions_arithmetic.js
+++ exercises/4.1/exercises/01_expressions_arithmetic.js
@@ -1,2 +1,14 @@
 let a = 8;
 let b = 5;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulus = a % b;
+
+console.log(`Addition ${a} + ${b} = ${addition}`);
+console.log(`Subtraction ${a} - ${b} = ${subtraction}`);
+console.log(`Multiplication ${a} * ${b} = ${multiplication}`);
+console.log(`Division ${a} / ${b} = ${division}`);
+console.log(`Modulus ${a} % ${b} = ${modulus}`);
