052ee3cb31b4d53521e64458d5b50ad061dab634
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 20:00:06 -0300

    Show the numbers

--- exercises/4.1/exercises/09_is_odd.js
+++ exercises/4.1/exercises/09_is_odd.js
@@ -2,3 +2,5 @@ let firstNumber = Math.ceil(Math.random() * 50);
 let secondNumber = Math.ceil(Math.random() * 50);
 let thridNumber = Math.ceil(Math.random() * 50);
 
+console.log('The numbers chosen were => ' + firstNumber + ", " + secondNumber + ', ' + thridNumber);
+
