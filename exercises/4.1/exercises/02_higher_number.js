7f9ebe42bc3736edb06e86ce2281691410a4c03d
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 05:51:09 -0300

    Correcting problems and ending exercise

--- exercises/4.1/exercises/02_higher_number.js
+++ exercises/4.1/exercises/02_higher_number.js
@@ -1,2 +1,13 @@
-let a = ceil(Math.random * 100);
-let b = ceil(Math.random * 100);
+let a = Math.ceil(Math.random() * 100);
+let b = Math.ceil(Math.random() * 100);
+
+console.log(`A = ${a}`);
+console.log(`B = ${b}`);
+
+if (a > b) {
+    console.log(`${a} is higher than ${b} !`);
+} else if (a < b) {
+    console.log(`${b} is higher than ${a} !`);
+} else {
+    console.log('Numbers equal!');
+}
