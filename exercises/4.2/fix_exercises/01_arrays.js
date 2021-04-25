9c9e6adacffdb81fb17105b42625e7f0cddabf83
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-25 20:02:05 -0300

    Get item index

--- exercises/4.2/fix_exercises/01_arrays.js
+++ exercises/4.2/fix_exercises/01_arrays.js
@@ -1,4 +1,7 @@
-let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
-let menuServices = menu[1];
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 
+let menuServices = menu[1];
 console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+console.log(indexOfPortfolio);
