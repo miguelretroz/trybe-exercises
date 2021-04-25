468f28f2fea27bd3672bd62b7afdb6d6ed6c539a
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-24 21:36:19 -0300

    Adds IR calculation

--- exercises/4.1/exercises/11_salary.js
+++ exercises/4.1/exercises/11_salary.js
@@ -1,8 +1,8 @@
 let grossSalary = 2850.50;
 
+//INSS
 let inssValue = 0;
 
-//INSS
 if (grossSalary < 1556.95) {
     inssValue = grossSalary * 0.08;
 } else if (grossSalary >= 1556.95 && grossSalary < 2594.93) {
@@ -11,4 +11,21 @@ if (grossSalary < 1556.95) {
     innsValue = grossSalary * 0.11;
 } else {
     inssValue = 570.88;
+}
+
+let salaryBase = grossSalary - inssValue;
+
+//IR
+let irValue = 0;
+
+if (salaryBase < 1903.99) {
+    irValue = 0;
+} else if (salaryBase >= 1903.99 && salaryBase < 2826.66) {
+    irValue = (salaryBase * 0.075) - 142.80;
+} else if (salaryBase >= 2826.66 && salaryBase < 3751.06) {
+    irValue = (salaryBase * 0.15) - 354.80;
+} else if (salaryBase >= 3751.06 && salaryBase <= 4664.68) {
+    irValue = (salaryBase * 0.225) - 636.13;
+} else {
+    irValue = (salaryBase * 0.275) - 869.36;
 }
