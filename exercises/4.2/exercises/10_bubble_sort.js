8c83af68f9162f5fb143167040e3d15e30276403
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-26 00:55:06 -0300

    4.2-> Exercise bonus-> Decreasing sort algorithm

--- exercises/4.2/exercises/10_bubble_sort.js
+++ exercises/4.2/exercises/10_bubble_sort.js
@@ -11,3 +11,15 @@ for (let index = 1; index < numbers.length; index += 1) {
 }
 
 console.log(numbers);
+
+for (let index = 1; index < numbers.length; index += 1) {
+    for (secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (numbers[index] > numbers[secondIndex]) {
+            let position = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = position;
+        }
+    }
+}
+
+console.log(numbers);
