060fdccdae6b3c43cc6f01153cd20ad38c7d0bec
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-25 23:20:57 -0300

    4.2-> Exercise 05-> Finding the higher number

--- exercises/4.2/exercises/05_higher_number.js
+++ exercises/4.2/exercises/05_higher_number.js
@@ -1,1 +1,11 @@
-let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let higher = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > higher) {
+        higher = numbers[index];
+    }
+}
+
+console.log(higher);
