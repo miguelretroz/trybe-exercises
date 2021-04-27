1e7ce81483c6eb37f4d9884e4221e70747513e71
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-27 05:59:37 -0300

    4.3-> Exercise 01-> Adds another logic to make a square

--- exercises/4.3/exercises/01_asterisk_square.js
+++ exercises/4.3/exercises/01_asterisk_square.js
@@ -13,3 +13,15 @@ for (let impreIndex = 0; impreIndex < n; impreIndex += 1) {
     }
     console.log(asterisksLine);
 };
+
+console.log();
+
+let asterisks02 = '';
+
+for (let stringIndex = 0; stringIndex < n; stringIndex += 1) {
+    asterisks02 += '*';
+}
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(asterisks02);
+}
