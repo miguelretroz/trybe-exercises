70444f0b701a76c9715547d3451867c05e3945a2
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-27 07:24:25 -0300

    4.3-> Exercise 04-> Adds spaces in string

--- exercises/4.3/exercises/04_pyramid.js
+++ exercises/4.3/exercises/04_pyramid.js
@@ -1,7 +1,17 @@
 let n = 5;
 
-let asteriskLine = [];
+let lineSpaces = [];
 
 for (let spaceIndex = 0; spaceIndex < (Math.ceil((n / 2))); spaceIndex += 1) {
-    asteriskLine.push('');
+    lineSpaces.push(' ');
+};
+
+for (let lineIndex = 1; lineIndex <= n; lineIndex += 2) {
+    lineSpaces.shift();
+
+    let pyramidLine = '';
+
+    for (space of lineSpaces) {
+        pyramidLine += space;
+    };
 };
