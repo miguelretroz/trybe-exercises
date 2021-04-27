b3d492b9e1576a889fd7e32eb9258de712455536
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-27 18:42:11 -0300

    4.4-> Fix exercise 01-> Adds new propertie to the array

--- exercises/4.4/fix_exercises/01_obj_player.js
+++ exercises/4.4/fix_exercises/01_obj_player.js
@@ -9,3 +9,8 @@ let player = {
 };
 
 console.log(`A jogadora ${player.name} ${player['lastName']} tem ${player.age} anos de idade.`);
+
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${player.bestInTheWorld.length} vezes.`);
+
