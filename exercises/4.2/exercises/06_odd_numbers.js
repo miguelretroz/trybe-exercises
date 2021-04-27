b03a1b23ea6b8cf6fc79731ed46b0a82ea092a5f
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-27 19:37:34 -0300

    ---Fix exercise 03---

--- exercises/4.2/exercises/06_odd_numbers.js
+++ exercises/4.2/exercises/06_odd_numbers.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let oddAmount = 0;
 
-for (number of numbers) {
+for (let number of numbers) {
     if ((number % 2) !== 0) {
         oddAmount += 1;
     };
