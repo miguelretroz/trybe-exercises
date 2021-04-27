4e0148fdd9d1759f8bab866c983463f707b0814c
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-27 05:53:57 -0300

    4.3-> Exercise 01-> Show the asterisks square

--- exercises/4.3/exercises/01_asterisk_square.js
+++ exercises/4.3/exercises/01_asterisk_square.js
@@ -5,3 +5,11 @@ let asterisks = [];
 for (let asterIndex = 0; asterIndex < n; asterIndex += 1) {
     asterisks.push('*');
 }
+
+for (let impreIndex = 0; impreIndex < n; impreIndex += 1) {
+    let asterisksLine = '';
+    for (asterisk of asterisks) {
+        asterisksLine += asterisk;
+    }
+    console.log(asterisksLine);
+};
