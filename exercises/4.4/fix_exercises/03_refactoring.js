2b25866ae48465ad7b8a889ec07969d2a853bff0
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 10:45:40 -0300

    4.4-> Fix exercise 03-> Adds functions for calculating the net salary

--- exercises/4.4/fix_exercises/03_refactoring.js
+++ exercises/4.4/fix_exercises/03_refactoring.js
@@ -283,3 +283,71 @@ O lucro de um produto é o resultado da subtração do valor de venda pelo custo
 
     console.log(`Profit per thousand sales -> U$ ${productProfit(costValue, saleValue, 1000)}`);
 };
+
+console.log();
+
+/* 11. Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+
+ INSS (Instituto Nacional do Seguro Social)
+   Salário bruto até R$ 1.556,94: alíquota de 8%
+   Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+   Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+   Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+
+ IR (Imposto de Renda)
+   Até R$ 1.903,98: isento de imposto de renda
+   De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+   De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+   De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+   Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto. */
+{
+    let grossSalary = 3000.00;
+
+    //INSS
+    function inss(salary) {
+        let inssValue = 0;
+        
+        if (salary < 1556.95) {
+            inssValue = salary * 0.08;
+        } else if (salary >= 1556.95 && salary < 2594.93) {
+            inssValue = salary * 0.09;
+        } else if (salary >= 2594.93 && salary <= 5189.82) {
+            inssValue = salary * 0.11;
+        } else {
+            inssValue = 570.88;
+        };
+
+        return inssValue;
+    };
+    
+    function salaryBase (salary) {
+        return (salary - inss(salary));
+    };
+    
+    //IR
+    function ir (salaryBase) {
+        let irValue = 0;
+        
+        if (salaryBase < 1903.99) {
+            irValue = 0;
+        } else if (salaryBase >= 1903.99 && salaryBase < 2826.66) {
+            irValue = (salaryBase * 0.075) - 142.80;
+        } else if (salaryBase >= 2826.66 && salaryBase < 3751.06) {
+            irValue = (salaryBase * 0.15) - 354.80;
+        } else if (salaryBase >= 3751.06 && salaryBase <= 4664.68) {
+            irValue = (salaryBase * 0.225) - 636.13;
+        } else {
+            irValue = (salaryBase * 0.275) - 869.36;
+        };
+
+        return irValue;
+    };
+
+    function salaryLiquid (salary) {
+        return salaryBase(salary) - ir(salaryBase(salary));
+    };
+
+    console.log(`O salário líquido é R$ ${salaryLiquid(grossSalary).toFixed(2)}`);
+};
