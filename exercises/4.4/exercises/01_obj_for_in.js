07b472ab3f79bb9db35548f9612147fa4693f999
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 11:25:46 -0300

    4.4-> Exercise 01-> Adds new propertie on the object

--- exercises/4.4/exercises/01_obj_for_in.js
+++ exercises/4.4/exercises/01_obj_for_in.js
@@ -2,8 +2,16 @@
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald'
 };
 
-// 1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+/* 1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. 
+
+Valor esperado no console: Bem-vinda, Margarida*/
 console.log(`Bem vinda, ${info.personagem}.`);
+
+console.log();
+
+/* 2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.*/
+info.recorrente = 'Sim';
+console.log(info);
