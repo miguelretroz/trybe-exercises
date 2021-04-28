85ae7945544d2bc3335c1a0f338fd92575c4266d
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 11:31:15 -0300

    4.4-> Exercise 01-> Showing the object properties key

--- exercises/4.4/exercises/01_obj_for_in.js
+++ exercises/4.4/exercises/01_obj_for_in.js
@@ -15,3 +15,11 @@ console.log();
 /* 2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.*/
 info.recorrente = 'Sim';
 console.log(info);
+
+console.log();
+
+// 3. Faça um for/in que mostre todas as chaves do objeto.
+for (let key in info) {
+    console.log(key);
+};
+
