0e9bf57da7dbf7a43543be5533c1df8a45b5a433
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 09:25:34 -0300

    4.4-> Fix exercise 03-> Adds function scoreVerification

--- exercises/4.4/fix_exercises/03_refactoring.js
+++ exercises/4.4/fix_exercises/03_refactoring.js
@@ -171,3 +171,37 @@ console.log();
 
     chessMovement(piece);
 };
+
+console.log();
+
+/* 7. Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
+{
+    let score = 99;
+
+    function scoreVerification (score) {    
+        if (score >= 90 && score <= 100) {
+            console.log('A');
+        } else if (score >= 80 && score < 90) {
+            console.log('B');
+        } else if (score >= 70 && score < 80) {
+            console.log('C');
+        } else if (score >= 60 && score < 70) {
+            console.log('D');
+        } else if (score >= 50 && score < 60) {
+            console.log('E');
+        } else if (score >= 0 && score < 50) {
+            console.log('F');
+        } else {
+            console.log('Invalid score!');
+        };
+    };
+
+    scoreVerification(score);
+};
