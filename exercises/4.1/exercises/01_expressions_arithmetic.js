0ebb8434931b4c97f82613224993fb90733380d9
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 08:41:16 -0300

    4.4-> Fix exercise 03-> Adds function higherNumber

--- exercises/4.1/exercises/01_expressions_arithmetic.js
+++ exercises/4.1/exercises/01_expressions_arithmetic.js
@@ -1,3 +1,10 @@
+/* 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
 let a = 8;
 let b = 5;
 
--- exercises/4.4/fix_exercises/03_refactoring.js
+++ exercises/4.4/fix_exercises/03_refactoring.js
@@ -1,14 +1,13 @@
+let numberA = 5;
+let numberB = 3;
+
 /* 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 Adição (a + b)
 Subtração (a - b)
 Multiplicação (a * b)
 Divisão (a / b)
 Módulo (a % b) */
-
-let numberA = 5;
-let numberB = 3;
-
-
+{
 // Sum
 function sum (number01, number02) {
     return (number01 + number02);
@@ -43,3 +42,21 @@ function modulus (number01, number02) {
 };
 
 console.log(modulus(numberA, numberB));
+};
+
+console.log();
+
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+{
+    function higherNumber (number01, number02) {
+        let higher = number01;
+
+        if (number02 > number01) {
+            higher = number02;
+        };
+        
+        return higher;
+    };
+
+    console.log(higherNumber(numberA, numberB));
+};
