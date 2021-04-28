73d0d0e25f6e31294f49e5f7c4b5f3dc53c0492d
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 11:53:08 -0300

    4.4-> Exercise 01-> Adds new object

--- exercises/4.4/exercises/01_obj_for_in.js
+++ exercises/4.4/exercises/01_obj_for_in.js
@@ -29,3 +29,21 @@ console.log();
 for (let key in info) {
     console.log(info[key]);
 };
+
+console.log();
+
+// 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info02 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (let key in info) {
+    if (key === 'recorrente' && (info[key] === info02[key])) { 
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + ' e ' + info02[key]);
+    };
+};
