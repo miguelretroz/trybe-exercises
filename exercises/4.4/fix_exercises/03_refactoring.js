8c7b2ea9caba5843f25827f8eacd44e9fdeeae32
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-28 08:21:54 -0300

    4.4-> Fix exercise 03-> Adds function sum

---
+++ exercises/4.4/fix_exercises/03_refactoring.js
@@ -0,0 +1,17 @@
+/* 1. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+let number01 = 5;
+let number02 = 3;
+
+
+//Sum
+function sum (number01, number02) {
+    return number01 + number02;
+}
+
+console.log(sum(number01, number02));
