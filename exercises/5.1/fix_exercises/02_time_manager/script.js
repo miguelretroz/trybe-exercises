4038e9779556ee773e842a12a0b5244c333938da
MiguelRetroz <miguel.retroz@outlook.com>
2021-04-29 19:37:31 -0300

    5.1-> Fix exercise 02-> Adds background color in sections

---
+++ exercises/5.1/fix_exercises/02_time_manager/script.js
@@ -0,0 +1,9 @@
+let header = document.getElementById('header-container');
+let emergencySection = document.getElementsByClassName('emergency-tasks')[0];
+let notEmergencySection = document.querySelector('.no-emergency-tasks');
+
+header.style.background = 'green';
+
+emergencySection.style.background = 'pink';
+
+notEmergencySection.style.background = 'yellow';
