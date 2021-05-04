d9415b112fbc000e4ab9b39ddd7108512bf38636
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-04 09:15:43 -0300

    5.3-> Exercise 01-> Adds days in the days list

--- exercises/5.3/exercise/scripts/script.js
+++ exercises/5.3/exercise/scripts/script.js
@@ -9,8 +9,51 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const dezHolidaysList = [24, 25, 31];
+const dezFridaysList = [4, 11, 18, 25];
+
+function holidayChecker(day, holidaysList) {
+    for (let holiday of holidaysList) {
+        if (day === holiday) {
+            return true;
+        };
+    };
+    return false;
+};
+
+function fridaysChecker(day, fridaysList) {
+    for (let friday of fridaysList) {
+        if (day === friday) {
+            return true;
+        };
+    };
+    return false;
+};
+
+function createCalendarDays(monthDayList) {
+    
+    let daysList = document.getElementById('days');
+
+    for (let day of monthDayList) {
+        let dayItem = document.createElement('li');
+        dayItem.className = 'day';
+        if (holidayChecker(day, dezHolidaysList)) {
+            dayItem.className += ' holiday';
+        };
+        if (fridaysChecker(day, dezFridaysList)) {
+            dayItem.className += ' friday';
+        };
+        dayItem.innerText = day;
+        daysList.appendChild(dayItem);
+    };
+};
+
+createCalendarDays(dezDaysList);
   
-  // Escreva seu código abaixo.
