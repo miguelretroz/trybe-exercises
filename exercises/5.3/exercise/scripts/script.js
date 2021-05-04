e7467d1f1a5463d5714231d07c95721e1907c639
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-04 11:29:24 -0300

    5.3-> Exercise 07-> Create function to add tasks

--- exercises/5.3/exercise/scripts/script.js
+++ exercises/5.3/exercise/scripts/script.js
@@ -172,3 +172,18 @@ function textZoomOut(event) {
 
 eventListApplier(daysListItens, 'mouseover', textZoomIn);
 eventListApplier(daysListItens, 'mouseout', textZoomOut);
+
+/* Exercício 7:
+Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+
+    O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" . */
+
+function taskCreator(taskName) {
+    let tasksContainer = document.getElementsByClassName('my-tasks')[0];
+    console.log(tasksContainer);
+    let task = document.createElement('span');
+    task.innerText = taskName;
+    tasksContainer.appendChild(task);
+};
+
+taskCreator('Cozinhar');
