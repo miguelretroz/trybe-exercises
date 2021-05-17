43d5096bfa9dab0a8f7e6e4063a7b9b6bad9ec1c
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-16 21:46:36 -0300

    6.1-> Exercise-> Changing the form verification

--- exercises/6.1/exercise/script.js
+++ exercises/6.1/exercise/script.js
@@ -1,22 +1,35 @@
 const selectState = document.getElementById('states');
 const states = [{name: 'Acre', initials: 'AC',}, {name: 'Alagoas', initials: 'AL',}, {name: 'Amapá', initials: 'AP',}, {name: 'Amazonas', initials: 'AM',}, {name: 'Bahia', initials: 'BA',}, {name: 'Ceará', initials: 'CE',}, {name: 'Espírito Santo', initials: 'ES',}, {name: 'Goiás', initials: 'GO',}, {name: 'Maranhão', initials: 'MA',}, {name: 'Mato Grosso', initials: 'MT',}, {name: 'Mato Grosso do Sul', initials: 'MS',}, {name: 'Minas Gerais', initials: 'MG',}, {name: 'Pará', initials: 'PA',}, {name: 'Paraíba', initials: 'PB',}, {name: 'Paraná', initials: 'PR',}, {name: 'Pernambuco', initials: 'PE',}, {name: 'Piauí', initials: 'PI',}, {name: 'Rio de Janeiro', initials: 'RJ',}, {name: 'Rio Grande do Norte', initials: 'RN',}, {name: 'Rio Grande do Sul', initials: 'RS',}, {name: 'Rondônia', initials: 'RO',}, {name: 'Roraima', initials: 'RR',}, {name: 'Santa Catarina', initials: 'SC',}, {name: 'São Paulo', initials: 'SP',}, {name: 'Sergipe', initials: 'SE',}, {name: 'Tocantins', initials: 'TO',}, {name: 'Distrito Federal', initials: 'DF',}];
-const dateInputBox = document.querySelector('#date');
 const btnSubmit = document.querySelector('#btn-submit');
+const body = document.querySelector('body');
+const script = document.querySelector('script');
+
+const nameInputBox = document.querySelector('#name');
+const emailInputBox = document.querySelector('#email');
+const cpfInputBox = document.querySelector('#cpf');
+const addressInputBox = document.querySelector('#address');
+const cityInputBox = document.querySelector('#city');
+const stateInput = document.querySelector('#states');
+
+const abstractInputBox = document.querySelector('#abstract');
+const jobInputBox = document.querySelector('#job');
+const descriptionInputBox = document.querySelector('#job-description');
+const dateInputBox = document.querySelector('#date');
 
 function createState(stateData) {
   const stateElement = document.createElement('option');
   stateElement.innerText = stateData.name;
   stateElement.setAttribute('value', stateData.initials);
   return stateElement;
-};
+}
 
 function addStateElements(statesDataList) {
   for (let state of statesDataList) {
     selectState.appendChild(createState(state));
   };
-};
+}
 
-function dateCheck() {
+function dateChecker() {
   let dateString = dateInputBox.value.split('/');
   let dateNumber = [];
   for (let number of dateString) {
@@ -30,8 +43,68 @@ function dateCheck() {
     alert('Mês inválido !');
   } else if ((dateNumber[2] < 0)) {
     alert('Ano inválido !');
+  };
+}
+
+function nameChecker() {
+  if (nameInputBox.value.length === 0 || nameInputBox.value.length > 40) {
+    return false;
+  };
+  return true;
+}
+
+function emailChecker() {
+  const emailLength = email.value.length;
+  let haveAt = 0;
+  if (emailLength !== 0 && emailLength <= 50) {
+    for (let char of emailInputBox.value) {
+      if (char === '@') {
+        haveAt += 1;
+      };
+    };
+    if (haveAt === 1) {
+      return true;
+    };
+  };
+  return false;
+}
+
+function cpfChecker() {
+  const cpfLength = cpfInputBox.value.length;
+  let haveLetter = false;
+  if (cpfLength > 0 && cpfLength <= 11) {
+    for (let number of cpfInputBox.value) {
+      if (isNaN(parseInt(number, 10))) {
+       return false;
+      };
+    };
+  };
+  return true;
+}
+ 
+
+function formValidation(event) {
+  event.preventDefault();
+  const div = document.createElement('div');
+  if (nameChecker()) {
+    if (emailChecker()) {
+      if (cpfChecker()) {
+        div.innerHTML = `<p>Nome: ${nameInputBox.value}</p>
+        <p>Email: ${emailInputBox.value}</p>
+        <p>CPF: ${cpfInputBox.value}</p>`;
+      } else {
+        div.innerHTML = '<h1>CPF inválido</h1>';
+      }
+    } else {
+      div.innerHTML = '<h1>Email inválido</h1>'
+    }
+  } else {
+    div.innerHTML = '<h1>Nome Inválido</h1>'
   }
+
+  body.insertBefore(div, script);
 }
 
 addStateElements(states);
-btnSubmit.addEventListener('click', dateCheck);
+// btnSubmit.addEventListener('click', dateCheck);
+btnSubmit.addEventListener('click', formValidation);
