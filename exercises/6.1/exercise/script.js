9733197c5616ddade92bb6b88ab8cd24b0e6f70a
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-18 09:41:45 -0300

    6.1-> Exercise-> Adds clear button

--- exercises/6.1/exercise/script.js
+++ exercises/6.1/exercise/script.js
@@ -1,9 +1,11 @@
 const selectState = document.getElementById('states');
 const states = [{name: 'Acre', initials: 'AC',}, {name: 'Alagoas', initials: 'AL',}, {name: 'Amapá', initials: 'AP',}, {name: 'Amazonas', initials: 'AM',}, {name: 'Bahia', initials: 'BA',}, {name: 'Ceará', initials: 'CE',}, {name: 'Espírito Santo', initials: 'ES',}, {name: 'Goiás', initials: 'GO',}, {name: 'Maranhão', initials: 'MA',}, {name: 'Mato Grosso', initials: 'MT',}, {name: 'Mato Grosso do Sul', initials: 'MS',}, {name: 'Minas Gerais', initials: 'MG',}, {name: 'Pará', initials: 'PA',}, {name: 'Paraíba', initials: 'PB',}, {name: 'Paraná', initials: 'PR',}, {name: 'Pernambuco', initials: 'PE',}, {name: 'Piauí', initials: 'PI',}, {name: 'Rio de Janeiro', initials: 'RJ',}, {name: 'Rio Grande do Norte', initials: 'RN',}, {name: 'Rio Grande do Sul', initials: 'RS',}, {name: 'Rondônia', initials: 'RO',}, {name: 'Roraima', initials: 'RR',}, {name: 'Santa Catarina', initials: 'SC',}, {name: 'São Paulo', initials: 'SP',}, {name: 'Sergipe', initials: 'SE',}, {name: 'Tocantins', initials: 'TO',}, {name: 'Distrito Federal', initials: 'DF',}];
 const btnSubmit = document.querySelector('#btn-submit');
+const btnClear = document.querySelector('#btn-clear');
 const body = document.querySelector('body');
 const script = document.querySelector('script');
 
+const form = document.querySelector('form');
 const nameInputBox = document.querySelector('#name');
 const emailInputBox = document.querySelector('#email');
 const cpfInputBox = document.querySelector('#cpf');
@@ -160,6 +162,14 @@ function dateError() {
   };
 }
 
+function clear() {
+  form.reset();
+  let divs = document.querySelectorAll('div');
+  for (let div of divs) {
+    body.removeChild(div);
+  }
+}
+
 function formValidation(event) {
   event.preventDefault();
   const div = document.createElement('div');
@@ -217,3 +227,4 @@ function formValidation(event) {
 addStateElements(states);
 // btnSubmit.addEventListener('click', dateCheck);
 btnSubmit.addEventListener('click', formValidation);
+btnClear.addEventListener('click', clear);
