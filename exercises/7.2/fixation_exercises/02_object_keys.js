9a4d169eb40b7948cedf5f4f54d6e7c116bfa678
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-24 09:34:53 -0300

    7.2-> Fixation exercise 02-> Add function to show the properties of objects

--- exercises/7.2/fixation_exercises/02_object_keys.js
+++ exercises/7.2/fixation_exercises/02_object_keys.js
@@ -12,3 +12,18 @@ const student2 = {
   SoftSkills: 'Ótimo',
   Git: 'Bom', // chave adicionada
 };
+
+function showProperties(object) {
+  const keys = Object.keys(object);
+
+  for (const key in keys) {
+    console.log(`${keys[key]}, Nível: ${object[keys[key]]}`);
+  };
+};
+
+console.log('---Estudante 01---');
+showProperties(student1);
+console.log();
+
+console.log('---Estudante 02---');
+showProperties(student2);
