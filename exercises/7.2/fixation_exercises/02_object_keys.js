277bd2dfa3fdf702e2a59b2f557ea93f9aaa7b03
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-24 09:21:38 -0300

    ---Fixation exercise 02---

---
+++ exercises/7.2/fixation_exercises/02_object_keys.js
@@ -0,0 +1,14 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
