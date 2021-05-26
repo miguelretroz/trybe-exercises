d7c95545690e24c9af399a03242d7885e74a52c7
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-26 11:03:58 -0300

    8.1-> Exercise 02-> Add function `lottery`

--- exercises/8.1/exercises/02_lottery.js
+++ exercises/8.1/exercises/02_lottery.js
@@ -3,3 +3,17 @@
 // como parâmetros o número apostado e uma função que checa se 
 //o número apostado é igual ao número sorteado.
 // O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const lottery = (number, callback) => {
+  const drawingNumber = Math.ceil(Math.random() * 5);
+  return callback(number, drawingNumber);
+}
+
+const lotteryResult = (number, lotteryNumber) => {
+  if (number === lotteryNumber) {
+    return 'Parabéns você ganhou';
+  }
+  return 'Tente novamente';
+};
+
+console.log(lottery(4, lotteryResult));
