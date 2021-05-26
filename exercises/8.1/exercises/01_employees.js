f6048329d7603ca8bb19647f6f61b971bfa45a0e
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-26 09:51:20 -0300

    8.1-> Exercise 01-> Add function to generate ids and emails to new employees

--- exercises/8.1/exercises/01_employees.js
+++ exercises/8.1/exercises/01_employees.js
@@ -2,3 +2,35 @@
 // Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id .
 // A sua função deve receber como parâmetro o nome completo da pessoa funcionária 
 // e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const emailGenerator = (name) => {
+  nameSplitted = name.toLowerCase().split(' ');
+  let email = '';
+  for (let index = 0; index < nameSplitted
+    .length; index += 1) {
+    if (index === 0) {
+      email += nameSplitted[0];
+    } else {
+      email += '_' + nameSplitted[index];
+    };
+  };
+  email += '@trybe.com';
+  return email;
+}
+
+const employee = (name) => {
+  return {
+    name,
+    email: emailGenerator(name),
+  }
+}
+
+const newEmployees = (callback) => {
+  return {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
+  };
+};
+
+console.log(newEmployees(employee));
