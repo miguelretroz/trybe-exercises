a5585843b75b024ff2b1af548540fa5e7c235939
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-26 11:33:41 -0300

    8.1-> Exercise 03-> Add function `answersResult`

--- exercises/8.1/exercises/03_answers_result.js
+++ exercises/8.1/exercises/03_answers_result.js
@@ -5,3 +5,22 @@
 
 //  - Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos,
 //    e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const answersResult = (answerSheet, answers, callback) => {
+  return `Score = ${callback(answerSheet, answers)}.`;
+}
+
+const answersChecker = (answersSheet, answers) => {
+  let score = 0;
+  for (let index = 0; index < answersSheet.length; index += 1) {
+    if (answersSheet[index] !== 'N.A' && answersSheet[index] === answers[index]) {
+      score += 1;
+    };
+  };
+  return score;
+}
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'C', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(answersResult(rightAnswers, studentAnswers, answersChecker));
