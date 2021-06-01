43b5dbb5abb10c32051586f484edef61837c1a17
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-31 21:29:57 -0300

    8.1-> Exercise bonus-> Add method `mageTurn` in the object `gameActions`

--- exercises/8.1/exercises/bonus_game_actions_simulator.js
+++ exercises/8.1/exercises/bonus_game_actions_simulator.js
@@ -22,16 +22,16 @@ const battleMembers = { mage, warrior, dragon };
 
 const damage = (min, max) =>  Math.round(min + Math.random() * (max - min));
 
-const mageDmg = (intelligence, mana, callback) => {
+const mageDmg = (intelligence, mana) => {
   if (mana >= 15) {
     return {
-      damage: callback(intelligence, (intelligence * 2)),
+      damage: damage(intelligence, (intelligence * 2)),
       manaSpending: 15,
     }
   }
   
   return {
-    damage: 'Não possui man suficiente',
+    damage: 'Não possui mana suficiente',
     manaSpending: 0,
   }
 }
@@ -40,6 +40,17 @@ const gameActions = {
   warriorTurn: (action) => {
     const damage = action(15, warrior.strength);
     warrior.damage = damage;
-    dragon.healthPoints = dragon.healthPoints - damage;
+    dragon.healthPoints -= damage;
+  },
+  mageTurn: (action) => {
+    const mageDamage = action(mage.intelligence, mage.mana);
+    if (typeof mageDamage.damage === 'number') {
+      dragon.healthPoints -= mageDamage.damage;
+      mage.mana -= mageDamage.manaSpending;
+      mage.damage = mageDamage.damage;
+    } else {
+      mage.damage = 0;
+      console.log(mageDamage.damage);
+    };
   },
 };
