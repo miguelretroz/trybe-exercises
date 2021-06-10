d25d3f4ab3b0782f133fca9735a843f174ed1127
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-10 09:04:57 -0300

    9.1-> Exercise 05-> Add function `sendMarsTemperature`

--- exercises/9.1/exercises/05-curiosity-actions.js
+++ exercises/9.1/exercises/05-curiosity-actions.js
@@ -10,6 +10,11 @@ const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const delay = messageDelay();
+  const marsTemperature = getMarsTemperature();
+  setTimeout(() => callback(marsTemperature), delay);
+}
 
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
