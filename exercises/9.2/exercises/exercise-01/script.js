c384d52e12c6c1cf48c748ae20969ebac6342da7
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-13 02:52:24 -0300

    9.2-> Exercise 01-> Adds `fetch` to getting API data

--- exercises/9.2/exercises/exercise-01/script.js
+++ exercises/9.2/exercises/exercise-01/script.js
@@ -3,6 +3,14 @@ const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' },
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(object => console.log(object));
 };
 
 window.onload = () => fetchJoke();
