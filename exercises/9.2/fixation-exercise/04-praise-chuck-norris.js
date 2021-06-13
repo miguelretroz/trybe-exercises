319113000be860381089e7df144d804d352aef50
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-12 22:34:31 -0300

    ---Fixation exercise 04---

---
+++ exercises/9.2/fixation-exercise/04-praise-chuck-norris.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+function verifiedFetch(url) {
+  return new Promise((resolve, reject) => {
+    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+      fetch(url)
+        .then(response => response.json())
+        .then(r => resolve(r.value));
+    }
+    else {
+      reject(new Error('endpoint não existe'));
+    }
+  });
+}
