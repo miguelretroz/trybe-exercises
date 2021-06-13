e3c01727b20c79dd4b2a5621791cc57493c8e451
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-12 22:14:45 -0300

    9.2-> Fixation exercise 03-> Add `catch`

--- exercises/9.2/fixation-exercise/03-promise-catch.js
+++ exercises/9.2/fixation-exercise/03-promise-catch.js
@@ -2,8 +2,8 @@ const promise = new Promise((resolve, reject) => {
   const number = Math.round((Math.random() * 11));
 
   if (number <= 5) {
-    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+    return reject(`Que fracasso =( Nosso número foi ${number}`);
   }
   resolve(number);
 }).then(number => `Que sucesso =) Nosso número foi ${number}`)
-  .then(msg => console.log(msg));
+  .then(msg => console.log(msg)).catch(msg => console.log(msg));
