8b4b690431d286f11b7a10adec90fe2711c12f75
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-14 20:50:53 -0300

    7.3-> Fixation exercise 02-> Add test to the function `division`

---
+++ exercises/7.3/fixation-exercises/02-division.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+function division(x, y) {
+  return x / y;
+}
+
+const expected = division(9, 3);
+
+assert.strictEqual(expected, 3, 'Nove dividido por três é igual a três');
