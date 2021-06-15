500c544ea872ac49843e5a65e5c025d507ab2b65
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-14 23:12:57 -0300

    7.3-> Fixation exercise 04-> Add tests for arrays and objects

---
+++ exercises/7.3/fixation-exercises/04-objects.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+const list1 = [1, 2, 3, 4, 5];
+const list2 = [1, 2, 3, 4, 5];
+
+assert.deepStrictEqual(list1, list2, 'Erro: list1 e list2 não são estritamente iguais');
+
+const person1 = { name: 'john', age: 21 };
+const person2 = { name: 'john', age: 21 };
+
+assert.deepStrictEqual(person1, person2, 'Erro: person1 e person2 não são estritamente iguais');
+
+const person3 = { name: 'john', age: 19 };
+
+assert.notDeepStrictEqual(person1, person3, 'Erro: os valores dos objetos são estritamente iguais');
