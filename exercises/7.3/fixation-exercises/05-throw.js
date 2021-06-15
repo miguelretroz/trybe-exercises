91954f85beb873cf712842c095de90ed6c3d92fa
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-14 23:45:38 -0300

    7.3-> Fixation exercise 05-> Add `throw`

--- exercises/7.3/fixation-exercises/05-throw.js
+++ exercises/7.3/fixation-exercises/05-throw.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
 
 function division(x, y) {
+  if (y === 0) throw new Error('parameter y must not be 0');
   return x / y;
 }
 
-assert.strictEqual(division(10, 10), 1);
-assert.strictEqual(division(10, 0), 0);
+assert.strictEqual(division(10, 2), 5);
+assert.throws( _ => { division(10, 0); }, /^Error: parameter y must not be 0$/);
