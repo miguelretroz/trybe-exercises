49de29694bf120c1d2c96c7d8a4c81f7b704dd75
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-15 18:28:26 -0300

    10.2-> Fixation exercise 02-> Add another example of how to use `done`

---
+++ exercises/10.2/fixation-exercises/02-sum-numbers.test.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@jest/globals');
+
+const SumNumbers = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  },500);
+};
+
+test('Testando SumNumbers, soma 5 mais 10', done => {
+  SumNumbers(5, 10, (result) => {
+    expect(result).toBe(15);
+    done();
+  });
+});
