f8371ef5126060f72a132591a4945016f8fdc32e
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-16 00:18:25 -0300

    10.2-> Exercise 02-> Add test to check if an error is throw

--- exercises/10.2/exercises/tests/02-users.test.js
+++ exercises/10.2/exercises/tests/02-users.test.js
@@ -1,12 +1,20 @@
 const { getUserName } = require('../src/02-users');
 const { describe, test, expect } = require('@jest/globals');
 
-describe('Testando getUserName', _ => {
+describe('Testando getUserName', () => {
   test('Buscando usuário por ID', () => {
     expect.assertions(1);
 
     return getUserName(4).then((name) => {
       expect(name).toBe('Mark');
+    });
+  });
+
+  test('Buscando usuário com ID inválido', () => {
+    expect.assertions(1);
+    const idToTest = 1;
+    return getUserName(idToTest).catch((error) => {
+      expect(error).toEqual({ error: `User with ${idToTest} not found.`});
     });
   });
 });
