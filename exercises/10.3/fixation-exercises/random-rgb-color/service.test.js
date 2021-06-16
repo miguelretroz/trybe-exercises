7231dca3f0dd660acf254153a0cf50a98e62667d
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-16 16:16:06 -0300

    10.3-> Fixation exercise 01-> Add `mockReturnValueOnce` and `toHaveBeenCalledTimes`

--- exercises/10.3/fixation-exercises/random-rgb-color/service.test.js
+++ exercises/10.3/fixation-exercises/random-rgb-color/service.test.js
@@ -3,9 +3,19 @@ const service = require('./service');
 
 test('#randomRgbColor', () => {
   service.randomRgbColor = jest.fn();
-  service.randomRgbColor.mockReturnValue('rgb(255, 255, 255)');
+  service.randomRgbColor
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
 
-  console.log(service.randomRgbColor());
-  expect(service.randomRgbColor).toHaveBeenCalled();
-  expect(service.randomRgbColor()).toBe('rgb(255, 255, 255)');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+
+  expect(service.randomRgbColor()).toBe('first call');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe('second call');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe('default value');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
 });
