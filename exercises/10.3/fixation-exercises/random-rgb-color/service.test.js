0dcec532e0d9b73f5b72f2aa8ac469ffd69eebbf
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-16 15:53:13 -0300

    10.3-> Fixation exercise 01-> Add test using `jest.fn()`

--- exercises/10.3/fixation-exercises/random-rgb-color/service.test.js
+++ exercises/10.3/fixation-exercises/random-rgb-color/service.test.js
@@ -2,5 +2,8 @@ const { test, expect } = require('@jest/globals');
 const service = require('./service');
 
 test('#randomRgbColor', () => {
+  service.randomRgbColor = jest.fn();
 
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
 });
