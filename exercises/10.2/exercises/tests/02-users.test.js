5c29c31c3901034c5e079b1ba3f38cbffc042dd7
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-16 00:26:43 -0300

    10.2-> Exercise 02-> Refactoring tests using `.resolves` and `.rejects`

--- exercises/10.2/exercises/tests/02-users.test.js
+++ exercises/10.2/exercises/tests/02-users.test.js
@@ -4,17 +4,14 @@ const { describe, test, expect } = require('@jest/globals');
 describe('Testando getUserName', () => {
   test('Buscando usuário por ID', () => {
     expect.assertions(1);
-
-    return getUserName(4).then((name) => {
-      expect(name).toBe('Mark');
-    });
+    return expect(getUserName(4)).resolves.toBe('Mark');
   });
 
   test('Buscando usuário com ID inválido', () => {
     expect.assertions(1);
     const idToTest = 1;
-    return getUserName(idToTest).catch((error) => {
-      expect(error).toEqual({ error: `User with ${idToTest} not found.`});
-    });
+    return expect(getUserName(idToTest))
+      .rejects
+      .toEqual({ error: `User with ${idToTest} not found.`});
   });
 });
