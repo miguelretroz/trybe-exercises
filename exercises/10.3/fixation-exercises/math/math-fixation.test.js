8923da49551d2cb1a2bf32b17ccc0cfd0ead1a75
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-20 14:11:31 -0300

    10.3-> Fixation exercise 07-> Add mock and test `math.subtrair`

--- exercises/10.3/fixation-exercises/math/math-fixation.test.js
+++ exercises/10.3/fixation-exercises/math/math-fixation.test.js
@@ -1,3 +1,16 @@
 const { describe, test, expect } = require('@jest/globals');
 
 const math = require('./math');
+
+describe('Para fixar...', () => {
+
+  test('Subtrair', () => {
+    math.subtrair = jest.fn();
+
+    expect(math.subtrair()).toBe(undefined);
+    math.subtrair.mockImplementation((a, b) => a * b);
+    expect(math.subtrair).toHaveBeenCalled();
+    expect(math.subtrair(1, 3)).toBe(3);
+    expect(math.subtrair).toHaveBeenCalledTimes(2);
+  });
+});
