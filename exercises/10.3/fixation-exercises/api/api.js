3ddbae17d5da355f4717db1641d1030173639016
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-20 13:27:26 -0300

    10.3-> Fixation exercise 06-> Add `fetchURL`

---
+++ exercises/10.3/fixation-exercises/api/api.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+function fetchURL() {
+  return fetch('https://ghibliapi.herokuapp.com/species').then(response =>
+  response
+    .json()
+    .then(json =>
+      response.ok ? Promise.resolve(json) : Promise.reject(json)
+    )
+  );
+}
+module.exports = { fetchURL };
