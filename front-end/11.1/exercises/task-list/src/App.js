224fc9d5e5a981200d5aee2a4fe72f6ceed07ca4
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-22 13:17:09 -0300

    11.1-> Exercise 01-> Add components using only array

--- front-end/11.1/exercises/task-list/src/App.js
+++ front-end/11.1/exercises/task-list/src/App.js
@@ -6,10 +6,13 @@ function Task (value) {
 
 const taskList = ['Estudar', 'Organizar agenda', 'Limpar mesa'];
 
+const elementsList = [<li>Element 01</li>, <li>Element 02</li>, <li>Element 03</li>];
+
 function App() {
   return (
     <ul>
       { taskList.map( task => Task(task))}
+      { elementsList }
     </ul>
   );
 }
