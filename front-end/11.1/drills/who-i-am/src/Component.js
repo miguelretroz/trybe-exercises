be24eb685517267546a8f38fb3a74dd119d6fef1
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-21 23:14:50 -0300

    11.1-> Drill 02-> Creating an component using `class`

---
+++ front-end/11.1/drills/who-i-am/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class WhoIAm extends React.Component {
+  render() {
+    return <div>
+      <h1>Miguel Retroz</h1>
+      <p>Sou estudante de desenvolvimento web pela Trybe!</p>
+    </div>
+  }
+}
+
+export default WhoIAm;
