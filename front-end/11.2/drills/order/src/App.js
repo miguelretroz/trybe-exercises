5eefe34225885981a2efe048e6797dd34d4ea2e4
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-25 12:56:40 -0300

    11.2-> Drill 02-> Create component `Order`

--- front-end/11.2/drills/order/src/App.js
+++ front-end/11.2/drills/order/src/App.js
@@ -1,10 +1,35 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;
