67a19a2b1b7656f7c74595d312028c827931a229
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-25 11:56:39 -0300

    11.2-> Drill 01-> Render Image

--- front-end/11.2/drills/image/src/App.js
+++ front-end/11.2/drills/image/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
 function App() {
   return (
     <div className="App">
+      <Image source={'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'} alternativeText={'Cute cat staring'} />
     </div>
   );
 }
