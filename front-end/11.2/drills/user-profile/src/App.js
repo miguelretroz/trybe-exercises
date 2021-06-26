90cb532b3e0cc8817a64f615b24b4a3874066170
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-26 20:31:03 -0300

    11.2-> Drill 03-> Refactoring component `App` using class

--- front-end/11.2/drills/user-profile/src/App.js
+++ front-end/11.2/drills/user-profile/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
