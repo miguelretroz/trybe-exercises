51963f3c7fa20841b9e52ce0367d58cd3d633edf
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 19:33:43 -0300

    12.1-> Drill 01-> Add more two buttons

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -5,11 +5,21 @@ function handleClick() {
   console.log('Clicou no primeiro botão!');
 }
 
+function handleClick02() {
+  console.log('Clicou no sugundo botão!');
+}
+
+function handleClick03() {
+  console.log('Clicou no terçeiro botão!');
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <button type="button" onClick={ handleClick }>1° Botão</button>
+        <button type="button" onClick={ handleClick02 }>2° Botão</button>
+        <button type="button" onClick={ handleClick03 }>3° Botão</button>
       </div>
     );
   }
