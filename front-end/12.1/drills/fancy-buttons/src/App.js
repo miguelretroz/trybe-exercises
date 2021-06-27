5e6c16ad25c7144824f5245c350f8e59d04059fc
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 16:52:20 -0300

    12.1-> Drill 01-> Add button with event

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function handleClick() {
+  console.log('Clicou no primeiro botão!');
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <button type="button" onClick={ handleClick }>1° Botão</button>
       </div>
     );
   }
