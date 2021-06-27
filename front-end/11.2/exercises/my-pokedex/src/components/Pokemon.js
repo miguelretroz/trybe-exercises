49225fb521aad7c4bcf232e2deffe31868b71180
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 00:48:18 -0300

    11.2-> Exercise Pokedex-> Add prop to `Image` render in `Pokemon`

--- front-end/11.2/exercises/my-pokedex/src/components/Pokemon.js
+++ front-end/11.2/exercises/my-pokedex/src/components/Pokemon.js
@@ -3,10 +3,10 @@ import Image from './Image';
 
 class Pokemon extends Component {
   render() {
-    const { name, type, averageWeight } = this.props.infos;
+    const { name, type, averageWeight, image } = this.props.infos;
     return (
       <div>
-        <Image />
+        <Image source={ image } alternativeText={ name } />
         <p>{ name }</p>
         <p>{ type }</p>
         <p>Average weight: { averageWeight.value }  { averageWeight.measurementUnit }</p>
