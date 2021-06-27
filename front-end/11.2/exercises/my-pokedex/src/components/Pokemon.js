e47469497d65051143d613ae7da3f5b95af56744
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 00:39:09 -0300

    11.2-> Exercise Pokedex-> Add props to `Pokemon`

--- front-end/11.2/exercises/my-pokedex/src/components/Pokemon.js
+++ front-end/11.2/exercises/my-pokedex/src/components/Pokemon.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import Image from './Image';
 
 class Pokemon extends Component {
   render() {
-    return;
+    const { name, type, averageWeight } = this.props.infos;
+    return (
+      <div>
+        <Image />
+        <p>{ name }</p>
+        <p>{ type }</p>
+        <p>Average weight: { averageWeight.value }  { averageWeight.measurementUnit }</p>
+      </div>
+    );
   }
 }
 
