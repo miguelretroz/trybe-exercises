4e1d921e1122401fc9194c323fc35262637941f3
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 16:46:55 -0300

    ---Drill 01---

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -1,10 +1,13 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
