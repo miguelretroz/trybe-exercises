803b48e0ef7092372f257a975b3c122bfa4bc610
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 01:16:55 -0300

    11.2-> Exercise Pokedex-> Render `Pokedex`

--- front-end/11.2/exercises/my-pokedex/src/App.js
+++ front-end/11.2/exercises/my-pokedex/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data';
+import Pokedex from './components/Pokedex';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Pokedex</h1>
+        <Pokedex pokemons={ data } />
       </div>
     );
   }
