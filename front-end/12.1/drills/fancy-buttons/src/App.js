802e6dae2e36107fb2ac887edfa80ebb3766adbe
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 00:38:58 -0300

    12.1-> Drill 01-> Changing buttons to increase the state

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      btn01: 0,
+      btn02: 0,
+      btn03: 0,
+    }
     this.handleClick = this.handleClick.bind(this);
   }
-  handleClick() {
-    console.log(this);
-    console.log('Clicou no primeiro botão!');
+  handleClick(state) {
+    this.setState((currentState, _props) => ({
+      [state]: currentState[state] + 1,
+    }));
   }
   render() {
     return (
       <div className="App">
-        <button type="button" onClick={ this.handleClick }>1° Botão</button>
+        <button type="button" onClick={ () => this.handleClick('btn01') }>{ this.state.btn01 }</button>
+        <button type="button" onClick={ () => this.handleClick('btn02') }>{ this.state.btn02 }</button>
+        <button type="button" onClick={ () => this.handleClick('btn03') }>{ this.state.btn03 }</button>
       </div>
     );
   }
