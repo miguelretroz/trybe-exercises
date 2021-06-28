ece60dec53002a74cca37bbe7b8e483525099a4b
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-27 23:52:00 -0300

    12.1-> Drill 01-> Moving function to into the class

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function handleClick() {
-  console.log('Clicou no primeiro botão!');
-}
-
-function handleClick02() {
-  console.log('Clicou no sugundo botão!');
-}
-
-function handleClick03() {
-  console.log('Clicou no terçeiro botão!');
-}
-
 class App extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    console.log(this);
+    console.log('Clicou no primeiro botão!');
+  }
   render() {
     return (
       <div className="App">
-        <button type="button" onClick={ handleClick }>1° Botão</button>
-        <button type="button" onClick={ handleClick02 }>2° Botão</button>
-        <button type="button" onClick={ handleClick03 }>3° Botão</button>
+        <button type="button" onClick={ this.handleClick }>1° Botão</button>
       </div>
     );
   }
