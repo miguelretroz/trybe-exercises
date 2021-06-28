574c2c50bb6de128a885259f887d7cc4a644f250
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 01:10:38 -0300

    12.1-> Drill 01-> Add bg color to `Button`

--- front-end/12.1/drills/fancy-buttons/src/components/Button.js
+++ front-end/12.1/drills/fancy-buttons/src/components/Button.js
@@ -5,18 +5,29 @@ class Button extends Component {
     super();
     this.state = {
       clicks: 0,
+      color: 'green',
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState((state, _props) => ({
-      clicks: state.clicks + 1,
-    }));
+    this.setState((state, _props) => {
+      let color = '';
+      if ((this.state.clicks % 2) === 0) {
+        color = 'red';
+      } else {
+        color = 'green';
+      }
+      console.log(color);
+      return {
+        clicks: state.clicks + 1,
+        color: color,
+      }
+    });
   }
 
   render() {
-    return <button type="button" onClick={ this.handleClick } >{ this.state.clicks }</button>;
+    return <button style={{ backgroundColor: this.state.color }}type="button" onClick={ this.handleClick } >{ this.state.clicks }</button>;
   }
 }
 
