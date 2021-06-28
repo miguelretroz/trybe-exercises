1c4f0061e24512198e4b975c8c7b5c06ec119069
MiguelRetroz <miguel.retroz@outlook.com>
2021-06-28 00:44:45 -0300

    12.1-> Drill 01-> Render three `Button`

--- front-end/12.1/drills/fancy-buttons/src/App.js
+++ front-end/12.1/drills/fancy-buttons/src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      btn01: 0,
-      btn02: 0,
-      btn03: 0,
-    }
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(state) {
-    this.setState((currentState, _props) => ({
-      [state]: currentState[state] + 1,
-    }));
-  }
   render() {
     return (
       <div className="App">
-        <button type="button" onClick={ () => this.handleClick('btn01') }>{ this.state.btn01 }</button>
-        <button type="button" onClick={ () => this.handleClick('btn02') }>{ this.state.btn02 }</button>
-        <button type="button" onClick={ () => this.handleClick('btn03') }>{ this.state.btn03 }</button>
+        <Button />
+        <Button />
+        <Button />
       </div>
     );
   }
