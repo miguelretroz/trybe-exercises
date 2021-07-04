597ba6811ed2f180932d31a969d1ab0c9a9e9132
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-04 04:17:43 -0300

    12.1-> Exercise Pokedex State-> Adds prop type to `Button`

--- front-end/12.1/exercises/pokedex-state/src/components/Button.jsx
+++ front-end/12.1/exercises/pokedex-state/src/components/Button.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   render() {
-    const { btnText, handleClick } = this.props;
+    const { btnName, btnValue, btnText, onClick } = this.props;
     return (
-      <button type="button" onClick={ handleClick } >{ btnText }</button>
+      <button name={ btnName } type="button" value={ btnValue } onClick={ onClick } >{ btnText }</button>
     );
   }
+}
+
+Button.defaultProps = {
+  btnName: '',
+  btnValue: '',
+}
+
+Button.propTypes = {
+  btnName: PropTypes.string,
+  btnValue: PropTypes.string,
+  btnText: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
 
 export default Button;
