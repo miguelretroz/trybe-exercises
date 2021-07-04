e01ade3e98604bb1eb4342f00058556cccb4ba60
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-04 03:29:09 -0300

    12.1-> Exercise Pokedex State-> Adds function to change `Pokedex` state

--- front-end/12.1/exercises/pokedex-state/src/components/Pokedex.js
+++ front-end/12.1/exercises/pokedex-state/src/components/Pokedex.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import Button from './Button';
 
 class Pokedex extends React.Component {
     constructor() {
-        super();
-        this.state = {
-            pkmIndex: 0,
-        }
+      super();
+      this.state = {
+        pkmIndex: 0,
+      }
+
+      this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick() {
+      const { pokemons } = this.props;
+      this.setState((prevState) => {
+        let { pkmIndex } = prevState;
+        pkmIndex += 1;
+        if (pkmIndex === pokemons.length) {
+          pkmIndex = 0;
+        }
+        return { pkmIndex };
+      })
+    }
+
     render() {
         const { pokemons } = this.props;
         const { pkmIndex } = this.state;
         return (
             <div className="pokedex">
                 <Pokemon key={pokemons[pkmIndex].id} pokemon={pokemons[pkmIndex]} />
+                <Button btnText={ 'Próximo pokemon' } handleClick={ this.handleClick } />
             </div>
         );
     }
