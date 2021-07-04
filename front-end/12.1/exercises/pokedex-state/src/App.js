05492107b5ddda5d19db4614750d226ff4eeef48
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-04 02:56:22 -0300

    12.1-> Exercise Pokedex State-> Refactoring `App` using class

--- front-end/12.1/exercises/pokedex-state/src/App.js
+++ front-end/12.1/exercises/pokedex-state/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './components/Pokedex';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
       <h1> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
