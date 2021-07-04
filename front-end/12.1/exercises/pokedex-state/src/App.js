926bb0b41e96557e3e7e9bc6b8853fefb559d37a
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-04 02:40:47 -0300

    12.1-> Exercise Pokedex State-> Creating directory `components` and moving `Pokedex` and `Pokemon`

--- front-end/12.1/exercises/pokedex-state/src/App.js
+++ front-end/12.1/exercises/pokedex-state/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-import Pokedex from './Pokedex';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
