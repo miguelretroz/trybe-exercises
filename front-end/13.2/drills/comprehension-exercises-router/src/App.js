457b17c64fea152f08fa5540a89a453284bb4c8d
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-05 16:15:15 -0300

    13.2-> Drill 01-> Adds `Route` to `Home`

--- front-end/13.2/drills/comprehension-exercises-router/src/App.js
+++ front-end/13.2/drills/comprehension-exercises-router/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Route path="/">
         <Home />
+        </Route>
       </BrowserRouter>
     );
   }
