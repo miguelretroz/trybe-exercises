38f4dc96ad16b3f1896bcb890f76e05b8865a080
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-05 16:17:36 -0300

    13.2-> Drill 01-> Adds `About` with route

--- front-end/13.2/drills/comprehension-exercises-router/src/App.js
+++ front-end/13.2/drills/comprehension-exercises-router/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/">
-        <Home />
+          <Home />
         </Route>
+        <Route path='/about' component={ About } />
       </BrowserRouter>
     );
   }
