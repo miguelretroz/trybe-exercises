6d60820ab3fdb8ee07db0c5fb972efd8e0c3b6e0
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-05 09:55:58 -0300

    ---Drill 01---

--- front-end/13.1/drills/my-interdimensional-app/src/App.js
+++ front-end/13.1/drills/my-interdimensional-app/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    }
+  }
+
   render() {
     return ();
   }
