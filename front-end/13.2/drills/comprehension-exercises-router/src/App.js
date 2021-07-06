61f1a5403f594532bb8bceb38ec44cc1a1dd00e2
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-05 21:32:24 -0300

    13.2-> Drill 01-> Adds Url params to `Users`

--- front-end/13.2/drills/comprehension-exercises-router/src/App.js
+++ front-end/13.2/drills/comprehension-exercises-router/src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
       <BrowserRouter>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
-        <Link to='/users'>Users</Link>
+        <Link to='/users/1'>Users</Link>
         <Route exact path="/">
           <Home />
         </Route>
         <Route path='/about' component={ About } />
-        <Route path='/users' render={props => (<Users {...props} greetingsMessage={ 'Good Morning' } />)} />
+        <Route path='/users/:id' render={props => (<Users {...props} greetingsMessage={ 'Good Morning' } />)} />
       </BrowserRouter>
     );
   }
--- front-end/13.2/drills/comprehension-exercises-router/src/components/Users.js
+++ front-end/13.2/drills/comprehension-exercises-router/src/components/Users.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingsMessage } = this.props;
+    const { greetingsMessage, match: { params: { id }} } = this.props;
+    console.log(id);
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { greetingsMessage }, My awesome Users component { id } </p>
       </div>
     );
   }
