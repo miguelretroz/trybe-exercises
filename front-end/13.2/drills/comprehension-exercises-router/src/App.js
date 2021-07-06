729ef6a7e9322a450a851766b8f3eb56156245d1
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-05 21:40:34 -0300

    13.2-> Drill 01-> Adds `Switch` in `App`

--- front-end/13.2/drills/comprehension-exercises-router/src/App.js
+++ front-end/13.2/drills/comprehension-exercises-router/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
@@ -12,11 +12,17 @@ class App extends Component {
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to='/users/1'>Users</Link>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path='/about' component={ About } />
-        <Route path='/users/:id' render={props => (<Users {...props} greetingsMessage={ 'Good Morning' } />)} />
+        <Switch>
+          <Route
+            path='/users/:id'
+            render={props => (
+              <Users {...props} greetingsMessage={ 'Good Morning' } />
+              )
+            }
+          />
+          <Route path='/about' component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
       </BrowserRouter>
     );
   }
