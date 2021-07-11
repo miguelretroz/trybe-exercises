9bda945223b61333d2ae406615b79c158bdc2d92
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-11 02:10:40 -0300

    12.2-> Drill 01-> Adds prop types in `Input`

--- front-end/12.2/drills/first-form/src/components/Input.js
+++ front-end/12.2/drills/first-form/src/components/Input.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Input extends Component {
   render() {
-    const { type, id, name, value, onChange } = this.props;
+    const { type, id, name, value} = this.props;
     return (
       <label htmlFor={ id }>
         <input
@@ -16,5 +17,13 @@ class Input extends Component {
     );
   }
 }
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default Input;
