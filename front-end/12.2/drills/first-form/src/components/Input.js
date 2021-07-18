301f6f5668d65336ed5048159ecd63bdc01e8581
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-18 19:34:12 -0300

    Add verification to the input component

--- front-end/12.2/drills/first-form/src/components/Input.js
+++ front-end/12.2/drills/first-form/src/components/Input.js
@@ -2,6 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Input extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      haveErrors: false,
+    };
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    const { onChange } = this.props;
+    const { target: { value, name } } = e;
+    this.setState({
+      haveErrors: value !== 'miguel',
+    }, () => {
+      console.log(value);
+      onChange({ target: {
+        value,
+        name,
+      } }, this.state);
+    });
+  }
+
   render() {
     const { type, id, name, value, children } = this.props;
     return (
@@ -12,7 +37,7 @@ class Input extends Component {
           id={ id }
           name={ name }
           value={ value }
-          onChange={ handleChange }
+          onChange={ this.handleChange }
         />
       </label>
     );
