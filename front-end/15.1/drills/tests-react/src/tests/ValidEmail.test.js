703407132b5acd98f598fef9b1de1f238d70267b
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 11:29:30 -0300

    15.1-> Drill 01-> Adding describe to `ValidEmail` tests

--- front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
+++ front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
@@ -1,16 +1,18 @@
 import { render } from '@testing-library/react';
 import ValidEmail from '../components/ValidEmail';
 
-test('Testando um componente, caso o email seja valido.', () => {
-  const EMAIL_USER = 'email@email.com';
-  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
-  const isValid = getByText('Email Valido');
-  expect(isValid).toBeInTheDocument();
-});
+describe('Testando componente "ValidEmail"', () => {
+  it('Testando um componente, caso o email seja valido.', () => {
+    const EMAIL_USER = 'email@email.com';
+    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const isValid = getByText('Email Valido');
+    expect(isValid).toBeInTheDocument();
+  });
 
-test('Testando um componente, caso o email seja inválido.', () => {
-  const EMAIL_USER = 'emailerrado';
-  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
-  const isValid = getByText('Email Inválido');
-  expect(isValid).toBeInTheDocument();
+  it('Testando um componente, caso o email seja inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const isValid = getByText('Email Inválido');
+    expect(isValid).toBeInTheDocument();
+  });
 });
