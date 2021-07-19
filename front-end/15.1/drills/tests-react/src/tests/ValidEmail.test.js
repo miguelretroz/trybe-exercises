a56959f0cf015bec01b128d33eb55f84b18815f8
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 12:16:00 -0300

    15.1-> Drill 01-> Adding test to check if the message has class `red-text`

--- front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
+++ front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
@@ -4,21 +4,21 @@ import ValidEmail from '../components/ValidEmail';
 describe('Testando componente "ValidEmail"', () => {
   it('Testando um componente, caso o email seja valido.', () => {
     const EMAIL_USER = 'email@email.com';
-    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const { getByText } = render(<ValidEmail email={ EMAIL_USER } />);
     const isValid = getByText('Email Válido');
     expect(isValid).toBeInTheDocument();
   });
 
   it('Testando um componente, caso o email seja inválido.', () => {
     const EMAIL_USER = 'emailerrado';
-    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const { getByText } = render(<ValidEmail email={ EMAIL_USER } />);
     const isValid = getByText('Email Inválido');
     expect(isValid).toBeInTheDocument();
   });
 
   it('Testando um componente, caso nada tenha sido enviado', () => {
     const EMAIL_USER = '';
-    const { queryByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const { queryByText } = render(<ValidEmail email={ EMAIL_USER } />);
     const isValid = queryByText('Email Válido');
     const isInvalid = queryByText('Email Inválido');
     expect(isValid).not.toBeInTheDocument();
@@ -27,8 +27,15 @@ describe('Testando componente "ValidEmail"', () => {
 
   it('Testando se o componente possui texto verde ao ser digitado um email válido.', () => {
     const EMAIL_USER = 'email@email.com';
-    const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+    const { getByText } = render(<ValidEmail email={ EMAIL_USER } />);
     const isValid = getByText('Email Válido');
     expect(isValid).toHaveAttribute('class', 'green-text');
+  });
+
+  it('Testando se o componente possui texto vermelho ao ser digitado um email inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    const { getByText } = render(<ValidEmail email={ EMAIL_USER } />);
+    const isInvalid = getByText('Email Inválido');
+    expect(isInvalid).toHaveAttribute('class', 'red-text');
   });
 });
