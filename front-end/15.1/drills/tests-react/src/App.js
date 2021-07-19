795044cd6e8fc9d9051e7932ed157bb5ed633602
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 09:59:55 -0300

    15.1-> Drill 01-> Refactoring `App` and adding events to the inputs

--- front-end/15.1/drills/tests-react/src/App.js
+++ front-end/15.1/drills/tests-react/src/App.js
@@ -1,16 +1,53 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <label htmlFor="id-email">
-        Email
-      </label>
-      <input id="id-email" type="email" />
-      <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
-      <input id="btn-back" type="button" value="Voltar" />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+        </label>
+        <input
+          id="id-email"
+          type="email"
+          value={ email }
+          onChange={(e) => this.changeEmail(e.target.value)}
+        />
+        <input
+          id="btn-send"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={() => this.changeSaveEmail(email)}
+        />
+        <input
+          id="btn-back"
+          type="button"
+          value="Voltar"
+        />
+        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
