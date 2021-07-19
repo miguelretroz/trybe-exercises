0965d105a21b4bc10fb721d7587a6f9f80dfb300
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 09:06:51 -0300

    15.1-> Drill 01-> Adds button `Voltar`

--- front-end/15.1/drills/tests-react/src/App.js
+++ front-end/15.1/drills/tests-react/src/App.js
@@ -8,6 +8,7 @@ function App() {
       </label>
       <input id="id-email" type="email" />
       <input id="btn-send" type="button" value="Enviar" />
+      <input id="btn-back" type="button" value="Voltar" />
     </div>
   );
 }
