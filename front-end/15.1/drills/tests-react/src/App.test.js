5c953836be059d49b119e05be8f7d20ebed29822
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 10:41:41 -0300

    15.1-> Drill 01-> Adds the first event test

--- front-end/15.1/drills/tests-react/src/App.test.js
+++ front-end/15.1/drills/tests-react/src/App.test.js
@@ -1,23 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('Verificando se existe o campo Email', () => {
-  const { getByLabelText } = render(<App />);
-  const inputEmail = getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail.type).toBe('email');
-});
+describe('Tela de inserção de email', () => {
+  it('Verificando se existe o campo Email', () => {
+    const { getByLabelText } = render(<App />);
+    const inputEmail = getByLabelText('Email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email');
+  });
+  
+  it('Verificando se existe um botão de enviar', () => {
+    const { getByTestId } = render(<App />);
+    const btn = getByTestId('id-send');
+    expect(btn).toBeInTheDocument();
+    expect(btn.type).toBe('button');
+    expect(btn).toHaveValue('Enviar');
+  });
+  
+  it('Verificando se existe dois botões', () => {
+    const { getAllByRole } = render(<App />);
+    const buttons = getAllByRole('button');
+    expect(buttons.length).toBe(2);
+  });
 
-test('Verificando se existe um botão de enviar', () => {
-  const { getByTestId } = render(<App />);
-  const btn = getByTestId('id-send');
-  expect(btn).toBeInTheDocument();
-  expect(btn.type).toBe('button');
-  expect(btn).toHaveValue('Enviar');
-});
+  it('Verifica que, ao inserir um email e clicar em "Enviar", o email aparece na tela', () => {
+    const { getByTestId, getByLabelText } = render(<App />);
 
-test('Verificando se existe dois botões', () => {
-  const { getAllByRole } = render(<App />);
-  const buttons = getAllByRole('button');
-  expect(buttons.length).toBe(2);
+    const EMAIL_USER = 'email@email.com';
+
+    const textEmail = getByTestId('id-email-user');
+    expect(textEmail).toBeInTheDocument();
+    expect(textEmail).toHaveTextContent('Valor:');
+
+    const btnSend = getByTestId('id-send');
+    const inputEmail = getByLabelText('Email');
+    fireEvent.change(inputEmail, { target: { value: EMAIL_USER }});
+    fireEvent.click(btnSend);
+    expect(inputEmail).toHaveValue('');
+    expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+  });
 });
