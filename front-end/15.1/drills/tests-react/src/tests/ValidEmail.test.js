a3334043ab6c4d17c794f298e027f0e2f99c2b4b
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 11:20:44 -0300

    15.1-> Drill 01-> Adding test for `ValidEmail` to check if email is invalid

--- front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
+++ front-end/15.1/drills/tests-react/src/tests/ValidEmail.test.js
@@ -7,3 +7,10 @@ test('Testando um componente, caso o email seja valido.', () => {
   const isValid = getByText('Email Valido');
   expect(isValid).toBeInTheDocument();
 });
+
+test('Testando um componente, caso o email seja inválido.', () => {
+  const EMAIL_USER = 'emailerrado';
+  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+  const isValid = getByText('Email Inválido');
+  expect(isValid).toBeInTheDocument();
+});
