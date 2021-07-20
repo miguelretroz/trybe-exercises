98206aafa612a3095568d4a84985743923395d8b
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 22:05:34 -0300

    15.2-> Drill 02-> Refactoring test events to use `user-event`

--- front-end/15.2/drills/input-test/src/App.test.js
+++ front-end/15.2/drills/input-test/src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('Testing App.js', () => {
@@ -7,12 +8,14 @@ describe('Testing App.js', () => {
 
     const inputName = screen.getByTestId('input-nome');
     expect(inputName).toHaveValue('');
-    fireEvent.change(inputName, { target: { value: 'Estudante da Trybe' } });
+    // fireEvent.change(inputName, { target: { value: 'Estudante da Trybe' } });
+    userEvent.type(inputName, 'Estudante da Trybe');
     expect(inputName).toHaveValue('Estudante da Trybe');
 
     const inputEmail = screen.getByTestId('input-email');
     expect(inputEmail).toHaveValue('');
-    fireEvent.change(inputEmail, { target: { value: 'estudante@trybe.com' } });
+    // fireEvent.change(inputEmail, { target: { value: 'estudante@trybe.com' } });
+    userEvent.type(inputEmail, 'estudante@trybe.com');
     expect(inputEmail).toHaveValue('estudante@trybe.com');
   });
 });
