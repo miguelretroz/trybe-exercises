a53955b7dff7b01e4e3a317192ff52c943f86e38
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-19 22:01:47 -0300

    15.2-> Drill 02-> Adding tests for both inputs

--- front-end/15.2/drills/input-test/src/App.test.js
+++ front-end/15.2/drills/input-test/src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing App.js', () => {
+  it('Testing the inputs', () => {
+    render(<App />);
+
+    const inputName = screen.getByTestId('input-nome');
+    expect(inputName).toHaveValue('');
+    fireEvent.change(inputName, { target: { value: 'Estudante da Trybe' } });
+    expect(inputName).toHaveValue('Estudante da Trybe');
+
+    const inputEmail = screen.getByTestId('input-email');
+    expect(inputEmail).toHaveValue('');
+    fireEvent.change(inputEmail, { target: { value: 'estudante@trybe.com' } });
+    expect(inputEmail).toHaveValue('estudante@trybe.com');
+  });
 });
