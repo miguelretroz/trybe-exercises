7e2dc55707ed2c625333a2c60de5fb379dfc49d1
MiguelRetroz <miguel.retroz@outlook.com>
2021-07-28 15:48:57 -0300

    16.4-> Comprehension exercise 01-> Adding action to fetch

--- front-end/16.4/comprehension-exercises/doguinhos-app/src/redux/actions/index.js
+++ front-end/16.4/comprehension-exercises/doguinhos-app/src/redux/actions/index.js
@@ -1,3 +1,4 @@
 export { default as getImage, GET_IMAGE } from './getImage';
 export { default as requestDog, REQUEST_IMAGE } from './requestDog';
 export { default as failedRequest, FAILED_REQUEST } from './failedRequest';
+export { fetchDog } from './fetchDog';
