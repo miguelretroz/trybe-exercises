116760626bc99ee0ee6e40393fd28ef6414fd826
MiguelRetroz <miguel.retroz@outlook.com>
2021-08-07 03:19:35 -0300

    Moving `Provider` to the index.js

--- front-end/16.5/comprehension-exercises/redux-tests/src/App.js
+++ front-end/16.5/comprehension-exercises/redux-tests/src/App.js
@@ -1,17 +1,13 @@
 // src/App.js
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import ButtonClicks from './components/ButtonClicks';
 import NumberClicks from './components/NumberClicks';
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
-          <ButtonClicks />
-          <NumberClicks />
-        </Provider>
+        <ButtonClicks />
+        <NumberClicks />
       </div>
     );
   }
