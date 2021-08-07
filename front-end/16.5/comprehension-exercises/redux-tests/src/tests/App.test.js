6798df5f558554264aa30c1a929b27df1c3f7b7d
MiguelRetroz <miguel.retroz@outlook.com>
2021-08-07 04:01:15 -0300

    Adding first test using redux

---
+++ front-end/16.5/comprehension-exercises/redux-tests/src/tests/App.test.js
@@ -0,0 +1,14 @@
+import { cleanup } from '@testing-library/react';
+import renderWithRedux from '../helpers/renderWithRedux';
+import App from '../App';
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText(/Clique Aqui/i);
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+});
