da0b063a560a8bd3be9f74fbe2cb1c17fb1951d1
MiguelRetroz <miguel.retroz@outlook.com>
2021-08-07 04:16:03 -0300

    16.5-> Comprehension exercise 01-> Testing the button click

--- front-end/16.5/comprehension-exercises/redux-tests/src/tests/App.test.js
+++ front-end/16.5/comprehension-exercises/redux-tests/src/tests/App.test.js
@@ -1,4 +1,5 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRedux from '../helpers/renderWithRedux';
 import App from '../App';
 
@@ -16,5 +17,16 @@ describe('testing clicks', () => {
     const { getByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
 
     expect(getByText('5')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText(/clique aqui/i);
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+
+    fireEvent.click(buttonAdicionar);
+    expect(queryByText('1')).toBeInTheDocument();
   });
 });
