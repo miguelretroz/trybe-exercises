bbb76bf70947506b7ed9b71026d3a310d9c19f37
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-12 17:32:42 -0300

    Exercise bonus 04-> Show pieces provided by HAL

--- back-end/20.2/exercises-bonus.sql
+++ back-end/20.2/exercises-bonus.sql
@@ -5,3 +5,10 @@ SELECT Piece, Price FROM Provides;
 SELECT * FROM Provides ORDER BY Price DESC LIMIT 5;
 
 SELECT Provider, Price FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
+
+SELECT
+	*
+FROM
+	Provides
+WHERE Provider = 'HAL'
+ORDER BY Price DESC;
