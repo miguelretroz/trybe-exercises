b8872d20c5c677c0371b06454216acb0c0a9d338
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-12 17:26:51 -0300

    Exercise bonus 03-> Show four pieces with the highest price

--- back-end/20.2/exercises-bonus.sql
+++ back-end/20.2/exercises-bonus.sql
@@ -3,3 +3,5 @@ USE PiecesProviders;
 SELECT Piece, Price FROM Provides;
 
 SELECT * FROM Provides ORDER BY Price DESC LIMIT 5;
+
+SELECT Provider, Price FROM Provides ORDER BY Price DESC LIMIT 4 OFFSET 3;
