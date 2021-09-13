c1e0a6ac0f113d06f08c28fb8335b0a865fa4d2d
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-13 10:03:38 -0300

    Comprehension exercise 01-> Show movies that meet the conditions

--- back-end/20.3/comprehension-exercises/comprehension-exercises-01.sql
+++ back-end/20.3/comprehension-exercises/comprehension-exercises-01.sql
@@ -14,3 +14,16 @@ WHERE active IS FALSE
 AND store_id = 2
 AND first_name <> 'KENNETH'
 ORDER BY first_name ASC;
+
+SELECT
+	title, description, release_year, replacement_cost
+FROM
+	film
+WHERE replacement_cost >= 18
+AND (
+rating  = 'G'
+OR rating = 'PG'
+OR rating = 'PG-13'
+)
+ORDER BY replacement_cost DESC, title ASC
+LIMIT 100;
