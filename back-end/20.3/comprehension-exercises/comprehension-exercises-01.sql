6f5f16d32a4d51a8e13ee762fd994ad06347be12
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-13 09:47:51 -0300

    Comprehension Exercises 01-> Show customers name

--- back-end/20.3/comprehension-exercises/comprehension-exercises-01.sql
+++ back-end/20.3/comprehension-exercises/comprehension-exercises-01.sql
@@ -5,3 +5,12 @@ SELECT
 FROM
 	customer
 WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
+
+SELECT
+	first_name
+FROM
+	customer
+WHERE active IS FALSE
+AND store_id = 2
+AND first_name <> 'KENNETH'
+ORDER BY first_name ASC;
