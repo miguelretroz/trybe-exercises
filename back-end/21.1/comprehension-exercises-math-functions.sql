27d91fdf0fa8eb64025757438120ff9c4d018fbe
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-16 10:58:57 -0300

    Comprehension exercises math functions-> Round number up

--- back-end/21.1/comprehension-exercises-math-functions.sql
+++ back-end/21.1/comprehension-exercises-math-functions.sql
@@ -8,3 +8,4 @@ SELECT ROUND(15.7515971, 5);
 SELECT FLOOR(39.494);
 
 -- 4. Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
+SELECT CEIL(85.234);
