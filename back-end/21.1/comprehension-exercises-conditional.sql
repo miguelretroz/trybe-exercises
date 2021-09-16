cd2bbb356d016a0fcbf24c6f74ed99da237d4a62
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-16 09:53:27 -0300

    Comprehension exercises conditional-> Show message for each rating

--- back-end/21.1/comprehension-exercises-conditional.sql
+++ back-end/21.1/comprehension-exercises-conditional.sql
@@ -8,3 +8,16 @@ SELECT
     ) AS 'conheço o filme?'
 FROM
   sakila.film;
+
+SELECT
+    title,
+    rating,
+    CASE
+        WHEN rating = 'G' THEN 'Livre para todos'
+        WHEN rating LIKE 'PG%' THEN 'Não recomendado para menores de 13 anos'
+        -- WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos',
+        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+        ELSE 'Proibido para menores de idade'
+    END AS público_alvo
+FROM
+    sakila.film;
