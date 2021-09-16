806d45da462f4a876ec83f5fc39351ce7bae3307
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-16 10:47:51 -0300

    ---Comprehension exercises math functions---

---
+++ back-end/21.1/comprehension-exercises-math-functions.sql
@@ -0,0 +1,7 @@
+-- 1. Monte uma query que gere um valor entre 15 e 20 .
+
+-- 2. Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
+
+-- 3. Estamos com uma média de 39.494 de vendas de camisas por mês. Qual é o valor aproximado para baixo dessa média?
+
+-- 4. Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes. Qual é o valor aproximado para cima dessa média?
