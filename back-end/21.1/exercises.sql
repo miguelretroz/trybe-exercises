fd8bb18b81b61fd2d63df11dbd03c68dc207aa61
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-19 16:38:07 -0300

    Exercise 18-> Show the employee's name, last name and contract time

--- back-end/21.1/exercises.sql
+++ back-end/21.1/exercises.sql
@@ -132,3 +132,9 @@ WHERE
     YEAR(HIRE_DATE) = 1987
 AND MONTH(HIRE_DATE) = 7;
 -- 18: Escreva uma query que exiba as seguintes informações de cada funcionário: nome , sobrenome , tempo que trabalha na empresa (em dias) .
+SELECT
+    FIRST_NAME,
+    LAST_NAME,
+    DATEDIFF(CURRENT_DATE(), HIRE_DATE)
+FROM
+    employees;
