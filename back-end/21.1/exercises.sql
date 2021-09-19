52fa1234ae8fba781b6ff816f9617e00a3a46980
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-19 15:10:31 -0300

    Exercise 02-> Show the difference between higher and lower salary

--- back-end/21.1/exercises.sql
+++ back-end/21.1/exercises.sql
@@ -5,3 +5,10 @@ SELECT
     MAX(SALARY)
 FROM
     employees;
+
+-- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
+SELECT
+    MAX(SALARY),
+    MIN(SALARY)
+FROM
+    employees;
