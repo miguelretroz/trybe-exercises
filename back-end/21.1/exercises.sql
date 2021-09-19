b2c141e2dfac5b5948f713bb652247e0e04aaf90
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-19 15:42:32 -0300

    Exercise 06-> Show amount of devs

--- back-end/21.1/exercises.sql
+++ back-end/21.1/exercises.sql
@@ -38,6 +38,14 @@ SELECT
 FROM
     employees;
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
+SELECT
+    COUNT(*)
+FROM
+    employees
+GROUP BY
+    JOB_ID
+HAVING
+    JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
