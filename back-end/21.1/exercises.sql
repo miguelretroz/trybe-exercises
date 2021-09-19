6eecf1eef4bc6809d9c1b7dce2449d76f75ef232
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-19 15:17:21 -0300

    Exercise 03-> Show average salary by job group

--- back-end/21.1/exercises.sql
+++ back-end/21.1/exercises.sql
@@ -12,3 +12,14 @@ SELECT
     MIN(SALARY)
 FROM
     employees;
+
+-- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
+SELECT
+    JOB_ID,
+    AVG(SALARY) SALARY_AVG
+FROM
+    employees
+GROUP BY
+    JOB_ID
+ORDER BY
+    SALARY_AVG DESC;
