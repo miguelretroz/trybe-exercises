46d56b955b1738ef4dcd7ae8cd225b1cf99bf333
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-09-19 15:46:29 -0300

    Exercise 07-> Show the total salary for each job

--- back-end/21.1/exercises.sql
+++ back-end/21.1/exercises.sql
@@ -47,6 +47,13 @@ GROUP BY
 HAVING
     JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
+SELECT
+    JOB_ID,
+    SUM(SALARY)
+FROM
+    employees
+GROUP BY
+    JOB_ID;
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
 -- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id .
