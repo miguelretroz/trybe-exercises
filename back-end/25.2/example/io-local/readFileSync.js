0fe30b150f55497694aecd73ce218a1b6d968c62
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-21 10:17:15 -0300

    ---Read File Sync Example---

---
+++ back-end/25.2/example/io-local/readFileSync.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+try {
+  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(`Error ao ler o arquivo: ${err.path}`);
+  console.log(err);
+}
