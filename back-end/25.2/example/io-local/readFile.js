f02ec5547c65215cc0fd5751f099d1afcfa11c3e
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-21 10:34:17 -0300

    ---Read File Promise---

--- back-end/25.2/example/io-local/readFile.js
+++ back-end/25.2/example/io-local/readFile.js
@@ -4,7 +4,7 @@ const nomeDoArquivo = 'meu-arquivo.txt';
 
 fs.readFile(nomeDoArquivo, 'utf8', (err, data) => {
   if (err) {
-    console.error(`Não foi possível ler o arquivo ${nomeDoArquive}\n Erro: ${err}`);
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
     process.exit(1);
   }
   console.log(`Conteúdo do arquivo: ${data}`);
