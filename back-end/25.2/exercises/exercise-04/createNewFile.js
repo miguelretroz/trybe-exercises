17b2640e41b5adea54cfe7d6df74b364ca452d37
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-23 13:45:33 -0300

    ---Exercise 04.04---

---
+++ back-end/25.2/exercises/exercise-04/createNewFile.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+
+function createNewFile () {
+  return fs.readFile('simpsons.json', 'utf-8')
+    .then((result) => {
+      const simpsons = JSON.parse(result);
+
+      const charactersNeeded = simpsons.filter(({ id }) => {
+        const idNumber = Number(id);
+
+        return (idNumber >= 1 && idNumber <= 4);
+      });
+
+      return fs.writeFile('simpsonFamily.json', JSON.stringify(charactersNeeded), { flag: 'wx' });
+    })
+    .then(() => console.log('Arquivo criado com sucesso!'))
+    .catch((err) => console.log(`Erro: ${err}`));
+}
+
+module.exports = createNewFile;
