bdb354549b009c87e8a7b310aaa7ba0ea799cd3c
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-23 13:56:12 -0300

    ---Exercise 04.05---

---
+++ back-end/25.2/exercises/exercise-04/addNewCharacter.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+
+async function addNewCharacter() {
+  try {
+    const fileContent = await fs.readFile('simpsonFamily.json', 'utf-8');
+
+    const simpsonFamily = JSON.parse(fileContent);
+
+    const newSimpsonFamily = [
+      ...simpsonFamily,
+      {
+        id: `${(simpsonFamily.length + 1)}`,
+        name: 'Nelson Muntz',
+      },
+    ];
+
+    await fs.writeFile('simpsonFamily.json', JSON.stringify(newSimpsonFamily));
+
+    console.log('Personagem adicionado com sucesso!');
+  } catch (err) {
+    console.log(`Erro: ${err}`);
+  }
+}
+
+module.exports = addNewCharacter;
