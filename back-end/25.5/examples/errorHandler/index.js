18043d4531f770fc577c696b5cc88230560c1de4
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-26 13:43:44 -0300

    Example Error-> Formatting the error

--- back-end/25.5/examples/errorHandler/index.js
+++ back-end/25.5/examples/errorHandler/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const fs = require('fs/promises');
+const errorMiddleware = require('./errorMiddleware');
 
 const app = express();
 
@@ -17,17 +18,27 @@ const PORT = 3000;
 
 app.get(
   '/:filename',
-  rescue(
-    async (req, res) => {
-      const file = await fs.readFile(`./${req.params.filename}`);
-      res.send(file.toString('utf-8'));
+  [
+    rescue(
+      async (req, res) => {
+        const file = await fs.readFile(`./${req.params.filename}`);
+        res.send(file.toString('utf-8'));
+      },
+    ),
+    (err, req, res, next) => {
+      if (err.code === 'ENOENT') {
+        const newError = new Error(err.message);
+        newError.code = 'file_not_found';
+        newError.status = 404;
+        return next(newError);
+      }
+
+      return next(err);
     },
-  ),
+  ],
 );
 
-app.use((err, _req, res, _next) => {
-  res.status(500).json({ error: `Erro: ${ err.message }` });
-});
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Aplicação ouvindo na porta ${PORT}`);
