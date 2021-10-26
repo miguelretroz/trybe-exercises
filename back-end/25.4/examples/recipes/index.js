553ffe93f7e696b091ba5e86d02372951567bc35
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-26 00:07:04 -0300

    Example Recipes-> Add PUT route

--- back-end/25.4/examples/recipes/index.js
+++ back-end/25.4/examples/recipes/index.js
@@ -49,6 +49,19 @@ app.get('/recipes/:id', (req, res) => {
   res.status(200).json(recipe);
 });
 
+app.put('/recipes/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  return res.status(204).end();
+});
+
 app.listen(3000, () => {
   console.log(`Aplicação ouvindo na porta ${PORT}`);
 });
