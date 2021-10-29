d044097093857dd11a2d10c35bac3ba550cf9030
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-29 13:36:58 -0300

    Exercise 2

--- back-end/25.4/exercises/gretting-api/index.js
+++ back-end/25.4/exercises/gretting-api/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/ping', (_req, res) => res.status(200).json({ messsage: 'pong' }));
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O nome é obrigatório!' });
+
+  res.status(200).json({ messsage: `Hello, ${name}!` });
+});
 
 app.listen(PORT, () => console.log(`🚀 Server is running on PORT ${PORT} `));
