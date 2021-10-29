4786d22bc6d4f0c1cd115a9a68ac3250e95e8822
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-29 20:41:25 -0300

    Exercise 08

--- back-end/25.4/exercises/simpsons-api/index.js
+++ back-end/25.4/exercises/simpsons-api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const bodyParser = require('body-parser');
 const fs = require('fs/promises');
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/simpsons', rescue(
   async (_req, res) => {
@@ -25,6 +28,26 @@ app.get('/simpsons/:id', rescue(
     if (!character) return res.status(404).json({ message: 'simpson not found' });
 
     return res.status(200).json(character);
+  },
+));
+
+app.post('/simpsons', rescue(
+  async (req, res) => {
+    const { id, name } = req.body;
+
+    if (!id || !name) return res.status(400).json({ message: '"id" and "name" is required!' });
+
+    const simpsons = JSON.parse(await fs.readFile('./simpsons.json', 'utf-8'));
+
+    const simpsonIndex = simpsons.findIndex((simpson) => simpson.id === `${id}`);
+
+    if (simpsonIndex !== -1) return res.status(409).json({ message: 'id already exists' });
+
+    simpsons.push({ id: `${id}`, name });
+
+    await fs.writeFile('./simpsons.json', JSON.stringify(simpsons));
+
+    res.status(204).end();
   },
 ));
 
