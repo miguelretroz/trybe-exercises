ea1e38aa9b6403b41b0b72f9d8fb312a3233344b
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-29 18:51:00 -0300

    Exercise 05 and 06

--- back-end/25.4/exercises/simpsons-api/index.js
+++ back-end/25.4/exercises/simpsons-api/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
 
 const app = express();
 
 const PORT = 3000;
+
+app.get('/simpsons', rescue(
+  async (_req, res) => {
+    const simpsons = JSON.parse(await fs.readFile('./simpsons.json', 'utf-8'));
+
+    res.status(200).json(simpsons);
+  },
+));
 
 app.listen(PORT, () => console.log(`🚀 Server is running on PORT ${PORT}`));
