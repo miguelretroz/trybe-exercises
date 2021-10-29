b8db5b82c72ac446f14464e6b1d173b4c1341e12
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-29 19:05:02 -0300

    Exercise 7

--- back-end/25.4/exercises/simpsons-api/index.js
+++ back-end/25.4/exercises/simpsons-api/index.js
@@ -14,4 +14,18 @@ app.get('/simpsons', rescue(
   },
 ));
 
+app.get('/simpsons/:id', rescue(
+  async (req, res) => {
+    const { id } = req.params;
+
+    const simpsons = JSON.parse(await fs.readFile('./simpsons.json', 'utf-8'));
+
+    const character = simpsons.find((simpson) => simpson.id === id);
+
+    if (!character) return res.status(404).json({ message: 'simpson not found' });
+
+    return res.status(200).json(character);
+  },
+));
+
 app.listen(PORT, () => console.log(`🚀 Server is running on PORT ${PORT}`));
