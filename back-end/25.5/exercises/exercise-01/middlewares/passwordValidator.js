f786d09785696f586fdea57d88bdee5b4736f9a3
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-30 20:26:52 -0300

    Exercise 01-> Create `passwordValidator` middleware

---
+++ back-end/25.5/exercises/exercise-01/middlewares/passwordValidator.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) => {
+  const { password } = req.body;
+
+  if (!password) return res.status(400).json({ message: '"password" is required!' });
+
+  if (
+    parseInt(password, 10) === NaN
+    || password.toString().length < 4
+    ||password.toString().length > 8
+  ) return res.status(400).json({ message: 'Invalid "password"' });
+
+  next();
+};
