0c35c29a45745bf300bc80b7d4d693af0e5e90eb
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-10-30 22:53:57 -0300

    Exercise 02-> Add `errorMiddleware`

---
+++ back-end/25.5/exercises/exercise-02/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const {
+  errorMiddleware,
+} = require('./middlewares');
+
+const app = express();
+
+const PORT = 3000;
+
+
+app.use(errorMiddleware);
+
+app.listen(PORT, () => console.log(`🚀 Server is running on PORT ${PORT}`));
