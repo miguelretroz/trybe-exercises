febd57e9652fb5da79ecef76cc3cecc843bffb58
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-01 17:18:19 -0300

    Exercise 04-> Add route to get all teams

--- back-end/25.5/exercises/exercise-04/routers/teamsRouters.js
+++ back-end/25.5/exercises/exercise-04/routers/teamsRouters.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const rescue = require('express-rescue');
 const fs = require('fs/promises');
 
 const {
@@ -29,6 +30,20 @@ router.post(
      res.status(201).json(newTeam);
     },
    ],
+);
+
+router.get(
+  '/',
+  [
+    readTeamsFile,
+    rescue(
+      (req, res) => {
+        const { teams } = req;
+
+        res.status(200).json(teams);
+      },
+    ),
+  ],
 );
 
 module.exports = router;
