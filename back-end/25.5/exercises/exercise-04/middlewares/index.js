2584893765935a900507af76515a56fdbdbbe92a
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-01 16:36:38 -0300

    Exercise 04-> Add middleware `countryValidator`

--- back-end/25.5/exercises/exercise-04/middlewares/index.js
+++ back-end/25.5/exercises/exercise-04/middlewares/index.js
@@ -1,5 +1,9 @@
 const nameValidator = require('./nameValidator');
+const initialsValidator = require('./initialsValidator');
+const countryValidator = require('./countryValidator');
 
 module.exports = {
   nameValidator,
+  initialsValidator,
+  countryValidator,
 };
