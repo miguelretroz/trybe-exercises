e2462421b50454b199d38d39aec17ac69c89a26d
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-02 05:47:37 -0300

    Example Model MySQL-> Add route to create new author

--- back-end/26.1/examples/model-mysql/index.js
+++ back-end/26.1/examples/model-mysql/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const Author = require('./models/Author');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
@@ -20,6 +23,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!author) return res.status(404).json({ message: 'Not found' });
 
   res.status(200).json(author);
+});
+
+app.post('/authors', async (req, res) => {
+  const { firstName, middleName, lastName } = req.body;
+
+  if (!Author.isValid(firstName, middleName, lastName)) {
+    return res.status(400).json({ message: 'Dado inválidos' });
+  }
+
+  await Author.create(firstName, middleName, lastName);
+
+  res.status(201).json({ message: 'Autor criado com sucesso!' });
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
