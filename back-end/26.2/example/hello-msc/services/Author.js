da76ff640297ec4608dd92413abecfb24e1ca2e5
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-11-15 11:48:06 -0300

    Example Author-> Refactoring service `Author`

--- back-end/26.2/example/hello-msc/services/Author.js
+++ back-end/26.2/example/hello-msc/services/Author.js
@@ -1,9 +1,34 @@
 const Author = require('../models/Author');
 
-const create = async (firstName, middleName, lastName) =>
-  Author.create(firstName, middleName, lastName);
+const create = async (firstName, middleName, lastName) => {
+  const existingAuthor = await Author.findByName(firstName, middleName, lastName);
 
-const findById = async (id) => Author.findById(id);
+  if (existingAuthor) {
+    return {
+      error: {
+        code: 'alreadyExists',
+        message: 'Uma pessoa autora já existe com esse nome completo',
+      },
+    };
+  }
+
+  return Author.create(firstName, middleName, lastName);
+};
+
+const findById = async (id) => {
+  const author = await Author.findById(id);
+
+  if (!author) {
+    return {
+      error: {
+        code: 'notFound',
+        message: `Não foi possível encontrar uma pessoa autora com o id ${id}`,
+      },
+    };
+  };
+
+  return author;
+};
 
 const getAll = async () => Author.getAll();
 
