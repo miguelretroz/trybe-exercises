2fe21698ceebc73679105d81c7add82e223ebcbb
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-06 15:50:25 -0300

    Exercise multer-> Add validations

--- back-end/27.2/exercises/multer-exercises/middlewares/upload.js
+++ back-end/27.2/exercises/multer-exercises/middlewares/upload.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
+const folderPath = path.resolve('uploads');
 
 const storage = multer.diskStorage({
   destination: (_req, _file, callback) => {
-    const folderPath = path.resolve('uploads');
 
     callback(null, folderPath);
   },
@@ -13,4 +14,27 @@ const storage = multer.diskStorage({
   },
 });
 
-module.exports = multer({ storage });
+const fileFilter = (_req, file, callback) => {
+  if (file.mimetype !== 'image/png') {
+    const err = new Error('Extension must be `png`');
+
+    err.statusCode = 403;
+
+    return callback(err);
+  }
+
+  const filesList = fs.readdirSync(folderPath);
+  const fileAlreadyExists = filesList.some((filename) => filename.split('-')[1] === file.originalname);
+
+  if (fileAlreadyExists) {
+    const err = new Error('File already exists');
+
+    err.statusCode = 409;
+
+    return callback(err);
+  }
+
+  callback(null, true);
+};
+
+module.exports = multer({ storage, fileFilter });
