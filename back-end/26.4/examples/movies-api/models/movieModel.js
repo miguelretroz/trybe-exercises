2f70ac8e72b2c277bbf8d141355b7966b0106d4a
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-31 00:38:11 -0300

    Implemet create in the movie model

---
+++ back-end/26.4/examples/movies-api/models/movieModel.js
@@ -0,0 +1,15 @@
+const mongoConnection = require('./connection');
+
+const create = async ({ title, directedBy, releaseYear }) => {
+  const moviesCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection('movies'));
+
+  const { insertedId: id } = await moviesCollection
+    .insertOne({ title, directedBy, releaseYear });
+
+  return { id };
+}
+
+module.exports = {
+  create,
+};
