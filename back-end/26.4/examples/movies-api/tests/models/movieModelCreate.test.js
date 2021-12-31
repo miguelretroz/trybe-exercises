c0ee466b61eac9bef28c87a7925af7d736f37c72
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-31 01:12:41 -0300

    Implement db connection mock

--- back-end/26.4/examples/movies-api/tests/models/movieModelCreate.test.js
+++ back-end/26.4/examples/movies-api/tests/models/movieModelCreate.test.js
@@ -1,13 +1,35 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+
+const mongoConnection = require('../../models/connection');
 
 const MoviesModel = require('../../models/movieModel');
 
 describe('Insere um novo filme no BD', () => {
+  let connectionMock = null;
+
   const payloadMovie = {
     title: 'Example Movie',
     directedBy: 'Jane Dow',
     releaseYear: 1999,
   }
+
+  before(() => {
+    const ID_EXAMPLE = '604cb554311d68f491ba5781';
+    const insertOne = async () => ({ insertedId: ID_EXAMPLE });
+    const collection = async () => ({ insertOne });
+    const db = async (databaseName) => ({ collection });
+    const getConnectionMock = async () => ({ db });
+
+    connectionMock = getConnectionMock()
+      .then((conn) => conn.db('model_example'));
+
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock)
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
 
   describe('quando é inserido com sucesso', () => {
     it('retorna um objeto', async () => {
