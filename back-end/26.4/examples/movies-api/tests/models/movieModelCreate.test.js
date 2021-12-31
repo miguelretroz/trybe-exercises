b59ebd0320d739eb0e4f25c38a1f621e65219ace
MiguelRetroz <contato.miguelretroz@gmail.com>
2021-12-30 23:02:33 -0300

    Example movies-> Implement `movieControllerCreate` tests

---
+++ back-end/26.4/examples/movies-api/tests/models/movieModelCreate.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+
+const MoviesModel = {
+  create: () => {},
+};
+
+describe('Insere um novo filme no BD', () => {
+  const payloadMovie = {
+    title: 'Example Movie',
+    directedBy: 'Jane Dow',
+    releaseYear: 1999,
+  }
+
+  describe('quando é inserido com sucesso', () => {
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  });
+});
