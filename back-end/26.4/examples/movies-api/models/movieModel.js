6f0714c6c509644248538c815e63bad3adb6b403
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-01 10:06:13 -0300

    Implement getAll model

--- back-end/26.4/examples/movies-api/models/movieModel.js
+++ back-end/26.4/examples/movies-api/models/movieModel.js
@@ -10,6 +10,16 @@ const create = async ({ title, directedBy, releaseYear }) => {
   return { id };
 }
 
+const getAll = async () => {
+  const moviesCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection('movies'));
+
+  const movies = await moviesCollection.find().toArray();
+
+  return movies;
+};
+
 module.exports = {
   create,
+  getAll,
 };
