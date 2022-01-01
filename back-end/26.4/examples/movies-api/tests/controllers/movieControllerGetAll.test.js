7928c504dbdbdf3a25dece130ee0537338853d5e
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-01 10:58:45 -0300

    Create test for getAll controller

---
+++ back-end/26.4/examples/movies-api/tests/controllers/movieControllerGetAll.test.js
@@ -0,0 +1,48 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const MoviesController = {
+  getAll: () => {},
+};
+const MoviesService = require('../../services/movieService');
+
+describe('Ao chamar o controller getAll', () => {
+  const request = {};
+  const response = {};
+
+  response.status = sinon.stub()
+    .returns(response);
+  response.json = sinon.stub()
+    .returns();
+
+    const movies = [
+      {
+        title: 'Darkest Hour',
+        directedBy: 'Joe Wright',
+        releaseYear: 2018,
+      },
+      {
+        title: 'The Dark Knight',
+        directedBy: 'Christopher Nolan',
+        releaseYear: 2008,
+      },
+    ];
+
+  before(async () => {
+    sinon.stub(MoviesService, 'getAll').resolves(movies);
+
+    await MoviesController.getAll(request, response);
+  });
+
+  after(() => {
+    MoviesService.getAll.restore();
+  });
+
+  it('retorna o status 200', async () => {
+    expect(response.status.calledWith(200)).to.be.true;
+  });
+
+  it('retorna o array de filmes', async () => {
+    expect(response.json.calledWith({ movies })).to.be.true;
+  });
+});
