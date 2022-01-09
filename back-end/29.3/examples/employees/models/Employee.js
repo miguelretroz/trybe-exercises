10478ed1c6213b1a21c1ffa55ceeb796a9f03734
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-09 16:10:32 -0300

    Exercise Books Api-> Implement Api

--- back-end/29.3/examples/employees/models/Employee.js
+++ back-end/29.3/examples/employees/models/Employee.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('Employee', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    firtsName: DataTypes.STRING,
+    firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     age: DataTypes.INTEGER,
   }, {
