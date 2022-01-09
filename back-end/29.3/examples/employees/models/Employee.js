5375f38057e61567f6c16241dbac25dfcf417652
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-09 15:16:43 -0300

    Exercise Books Api-> Create model `Employee`

---
+++ back-end/29.3/examples/employees/models/Employee.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    firtsName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  }, {
+    timestamps: false,
+    tableName: 'Employees',
+    underscored: true,
+  });
+
+  Employee.associate = (models) => {
+    Employee.hasOne(models.Address,
+      { foreignKey: 'employee_id', as: 'addresses' });
+  };
+
+  return Employee;
+};
