4550678fdf93a11a73abff8e73fe601e5b0e4e85
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-09 22:44:09 -0300

    Example Books-> Create migrations

---
+++ back-end/29.3/examples/books/migrations/20220110012747-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'Users',
+      {
+        userId: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+          field: 'book_id',
+        },
+        firstName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          field: 'first_name',
+        },
+        lastName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          field: 'last_name',
+        },
+        age: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ back-end/29.3/examples/books/migrations/20220110012811-create-user-books.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'UserBooks',
+      {
+        userId: {
+          type: Sequelize.INTEGER,
+          field: 'user_id',
+          references: {
+            model: 'Users',
+            key: 'user_id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          primaryKey: true,
+        },
+        bookId: {
+          type: Sequelize.INTEGER,
+          field: 'book_id',
+          references: {
+            model: 'Books',
+            key: 'book_id',
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE',
+          primaryKey: true,
+        },
+      },
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UserBooks');
+  }
+};
