ce8e6510530a43aa28574d048803a5284a3feaf9
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-09 21:56:51 -0300

    Exercise Books Api-> Add route with `lazy loading`

--- back-end/29.3/examples/employees/index.js
+++ back-end/29.3/examples/employees/index.js
@@ -16,6 +16,28 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+      where: { id },
+    });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
