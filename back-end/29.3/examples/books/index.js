beb9f678fa61b12000a9c81607adef74451a3796
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-09 22:12:52 -0300

    Example Books-> Create api structure

---
+++ back-end/29.3/examples/books/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+module.exports = app;
