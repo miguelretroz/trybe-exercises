353daf7432cb8b329fa5c26864719a97b7f37712
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-01-15 18:19:58 -0300

    Exercise 02-> Part 02

--- back-end/30.1/exercises/exercício-2-piadas-chucknorris/index.js
+++ back-end/30.1/exercises/exercício-2-piadas-chucknorris/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const CategoriesController = require('./controllers/categories');
+const JokeController = require('./controllers/joke');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.get('/', (_req, res) => {
   res.redirect('/categories');
 });
 app.get('/categories', CategoriesController.listCategories);
+app.get('/jokes', JokeController.listRandom);
+app.get('/jokes/:category', JokeController.listByCategory);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
