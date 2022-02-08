8e192d7e38ba2fbab7a0690a348b329c262b07f7
MiguelRetroz <contato.miguelretroz@gmail.com>
2022-02-08 18:42:26 -0300

    Create people example

--- computer-science/32.1/examples/area.py
+++ computer-science/32.1/examples/area.py
@@ -14,6 +14,7 @@ def circle(radius):
     '''Calculate area of circle.'''
     return PI * radius * radius
 
-print("Área do quadrado:", square(10))
-print("Área do retângulo:", rectangle(2, 2))
-print("Área do círculo:", circle(3))
+if __name__ == "__main__":
+    print("Área do quadrado:", square(10))
+    print("Área do retângulo:", rectangle(2, 2))
+    print("Área do círculo:", circle(3))
