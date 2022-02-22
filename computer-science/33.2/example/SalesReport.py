04870c5bd5217146c1fd1c6405b52fbde0747403
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-02-22 15:48:03 -0300

    Example-> Refactor to use inheritance

--- computer-science/33.2/example/SalesReport.py
+++ computer-science/33.2/example/SalesReport.py
@@ -1,26 +1,32 @@
+from abc import ABC, abstractmethod
 import json
 
 
-class SalesReport():
+class SalesReport(ABC):
     def __init__(self, export_file):
-        self.export_file = export_file + '.json'
+        self.export_file = export_file
 
     def build(self):
         """ Aqui colocamos a lógica para a entidade 'se criar',
         ou seja, criar um relatório imprimível. Por simplicidade,
         vamos omitir essa lógica nos exemplos! """
         return [{
-            'Coluna 1': 'Dado 1',
-            'Coluna 2': 'Dado 2',
-            'Coluna 3': 'Dado 3'
+            "Coluna 1": "Dado 1",
+            "Coluna 2": "Dado 2",
+            "Coluna 3": "Dado 3"
             },
             {
-            'Coluna 1': 'Dado A',
-            'Coluna 2': 'Dado B',
-            'Coluna 3': 'Dado C'
+            "Coluna 1": "Dado A",
+            "Coluna 2": "Dado B",
+            "Coluna 3": "Dado C"
             }]
 
+    @abstractmethod
     def serialize(self):
-        # Vamos gerar, aqui, o nosso relatório em formato JSON
-        with open(self.export_file, 'w') as file:
+        raise NotImplementedError
+
+
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + ".json", "w") as file:
             json.dump(self.build(), file)
