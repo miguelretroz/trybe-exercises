2711150a16f69a9b214314ded71057647b46014d
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-13 14:58:03 -0300

    Example First Data Scraping-> Detail

--- computer-science/34.3/examples/crawler/first_data_scraping.py
+++ computer-science/34.3/examples/crawler/first_data_scraping.py
@@ -7,9 +7,20 @@ next_page_url = "page-1.html"
 while next_page_url:
     response = requests.get(f"{URL_BASE}{next_page_url}")
     selector = Selector(text=response.text)
+
     for product in selector.css(".product_pod"):
         title = product.css("h3 a::attr(title)").get()
         price = product.css(".price_color::text").get()
         print(title, price)
+
+        detail_href = product.css(".product_pod h3 a::attr(href)").get()
+        detail_page_url = f"{URL_BASE}{detail_href}"
+
+        detail_response = requests.get(detail_page_url)
+        detail_selector = Selector(text=detail_response.text)
+
+        detail_description = detail_selector.css(
+            '#product_description ~ p::text').get()
+        print(detail_description)
 
     next_page_url = selector.css(".next a::attr(href)").get()
