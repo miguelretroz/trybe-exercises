ab7782696a661d83c1f2b1e4ed0c68c4d65597c0
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-21 17:43:14 -0300

    Exercises Measurement Converter Scripts-> area

---
+++ typescript/day-01/exercises/measurement-converter-scripts/area.ts
@@ -0,0 +1,13 @@
+const conversionTableInSquareMeter: { [key: string]: number } = {
+  'k2m': 1000,
+  'hm²': 100,
+  'dam²': 10,
+  'm²': 1,
+  'dm²': 0.1,
+  'cm²': 0.01,
+  'mm²': 0.001,
+};
+
+export function convert(value: number, unitBase: string, conversionUnit: string): number {
+  return (value * conversionTableInSquareMeter[unitBase]) / conversionTableInSquareMeter[conversionUnit];
+}
