b0fa9f8ad66fe104595c5e5d543605e31dcd4082
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-22 19:12:55 -0300

    Example Selection Sort

---
+++ computer-science/35.3/examples/sorting_algorithms/selection_sort.py
@@ -0,0 +1,14 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+
+        array[minimum], array[i] = array[i], array[minimum]
+
+    return array
+
+
+print(selection_sort([100, 4, 6, 33, 56, 67]))
