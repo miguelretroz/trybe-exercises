a3ca9a87e8e3f803fe56299b1ee6a8f7199f8058
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-22 00:03:34 -0300

    Exercises Measurement Converter Scripts-> mass exec function

--- typescript/day-01/exercises/measurement-converter-scripts/mass.ts
+++ typescript/day-01/exercises/measurement-converter-scripts/mass.ts
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync';
+
 const conversionTableInGrass: { [key: string]: number } = {
   kg: 1000,
   hg: 100,
@@ -11,3 +13,27 @@ const conversionTableInGrass: { [key: string]: number } = {
 export function convert(value: number, unitBase: string, conversionUnit: string): number {
   return (value * conversionTableInGrass[unitBase]) / conversionTableInGrass[conversionUnit];
 }
+
+export function exec(): void {
+  const unitTable = Object.keys(conversionTableInGrass);
+
+  console.log('---Conversor de capacidade---');
+  const value: number = readlineSync.questionInt('Entre com o valor a ser convertido: ');
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const unitBaseNumber: string = readlineSync.keyIn('Escolha uma unidade base: ', { limit: '$<0-6>' });
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const conversionUnitNumber: string = readlineSync.keyIn('Entre com a unidade de conversão: ', { limit: '$<0-6>' });
+
+  const unitBase = unitTable[parseInt(unitBaseNumber)];
+  const conversionUnit = unitTable[parseInt(conversionUnitNumber)];
+
+  const valueConverted = convert(value, unitBase, conversionUnit);
+
+  console.log(`${value}${unitBase} é igual a ${valueConverted}${conversionUnit}`);
+}
+
+exec();
