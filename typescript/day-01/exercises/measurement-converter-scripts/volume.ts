e74cb5c91c24ec1c7b140f043d04e7f118492478
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-22 00:03:55 -0300

    Exercises Measurement Converter Scripts-> volume exec function

--- typescript/day-01/exercises/measurement-converter-scripts/volume.ts
+++ typescript/day-01/exercises/measurement-converter-scripts/volume.ts
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync';
+
 const conversionTableInCubicMeter: { [key: string]: number } = {
   'km³': 1000,
   'hm³': 100,
@@ -11,3 +13,27 @@ const conversionTableInCubicMeter: { [key: string]: number } = {
 export function convert(value: number, unitBase: string, conversionUnit: string): number {
   return (value * conversionTableInCubicMeter[unitBase]) / conversionTableInCubicMeter[conversionUnit];
 }
+
+export function exec(): void {
+  const unitTable = Object.keys(conversionTableInCubicMeter);
+
+  console.log('---Conversor de capacidade---');
+  const value: number = readlineSync.questionInt('Entre com o valor a ser convertido: ');
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const unitBaseNumber: string = readlineSync.keyIn('Escolha uma unidade base: ', { limit: '$<0-6>' });
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const conversionUnitNumber: string = readlineSync.keyIn('Entre com a unidade de conversão: ', { limit: '$<0-6>' });
+
+  const unitBase = unitTable[parseInt(unitBaseNumber)];
+  const conversionUnit = unitTable[parseInt(conversionUnitNumber)];
+
+  const valueConverted = convert(value, unitBase, conversionUnit);
+
+  console.log(`${value}${unitBase} é igual a ${valueConverted}${conversionUnit}`);
+}
+
+exec();
