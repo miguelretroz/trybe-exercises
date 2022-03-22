8b1a0b69390c56eff04a5dfcf546006888ddb4f9
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-21 23:26:31 -0300

    Exercises Measurement Converter Scripts-> area exec function

--- typescript/day-01/exercises/measurement-converter-scripts/area.ts
+++ typescript/day-01/exercises/measurement-converter-scripts/area.ts
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync';
+
 const conversionTableInSquareMeter: { [key: string]: number } = {
   'km²': 1000,
   'hm²': 100,
@@ -11,3 +13,27 @@ const conversionTableInSquareMeter: { [key: string]: number } = {
 export function convert(value: number, unitBase: string, conversionUnit: string): number {
   return (value * conversionTableInSquareMeter[unitBase]) / conversionTableInSquareMeter[conversionUnit];
 }
+
+export function exec(): void {
+  const unitTable = ['km²', 'hm²', 'dam²', 'm²', 'dm²', 'cm²', 'mm²'];
+
+  console.log('---Conversor de área---');
+  const value: number = readlineSync.questionInt('Entre com o valor a ser convertido: ');
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const unitBaseNumber: string = readlineSync.keyIn('Escolha uma unidade base: ', { limit: '$<0-6>' });
+
+  console.log('\nTabela de unidades');
+  console.table(unitTable);
+  const conversionUnitNumber: string = readlineSync.keyIn('Entre com a unidade de conversão: ', { limit: '$<0-6>' });
+
+  const unitBase = unitTable[parseInt(unitBaseNumber)];
+  const conversionUnit = unitTable[parseInt(conversionUnitNumber)];
+
+  const valueConverted = convert(value, unitBase, conversionUnit);
+
+  console.log(`${value}${unitBase} é igual a ${valueConverted}${conversionUnit}`);
+}
+
+exec();
