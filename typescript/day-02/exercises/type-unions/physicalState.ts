f6276191e1e129011ccd0330dc15c8bd84d0bcef
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-27 19:51:35 -0300

    Exercises Type Unions-> Create type PhysicalState

---
+++ typescript/day-02/exercises/type-unions/physicalState.ts
@@ -0,0 +1,10 @@
+type PhysicalState = 'liquid' | 'solid' | 'gas';
+
+const liquidMatter: PhysicalState = 'liquid';
+const solidMatter: PhysicalState = 'solid';
+const gasMatter: PhysicalState = 'gas';
+
+
+console.log(liquidMatter);
+console.log(solidMatter);
+console.log(gasMatter);
