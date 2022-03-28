16267b1fed3b399c8ea4150fc67795a52db73352
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-28 03:13:14 -0300

    Exercise 04 Car-> Create Car class

---
+++ typescript/day-02/exercises/car/Car.ts
@@ -0,0 +1,66 @@
+enum Colors {
+  Black = "Preta",
+  White = "Branca",
+  Red = "Vermelha",
+  Silver = "Prata"
+}
+
+enum Doors {
+  FrontLeft = "frente esquerda",
+  FrontRight = "frente direita",
+  FearLeft = "traseira esquerda",
+  FearRight = "traseira direita",
+}
+
+enum Directions {
+  Left = "Esquerda",
+  Right = "Direita"
+}
+
+class Car {
+  _brand: string;
+  _color: Colors;
+  _doors: number;
+
+  constructor(brand: string, color: Colors, doors: number) {
+    this._brand = brand;
+    this._color = color;
+    this._doors = doors;
+  }
+
+  honk(): void {
+    console.log("Biiiiii!");
+  }
+
+  openTheDoor(door: Doors): void {
+    console.log(`A porta ${door} foi aberta!`);
+  }
+
+  closeTheDoor(door: Doors): void {
+    console.log(`A porta ${door} foi fechada!`);
+  }
+
+  turnOn(): void {
+    console.log("Carro ligado!");
+  }
+
+  turnOff(): void {
+    console.log("Carro desligado!");
+  }
+
+  speedUp(): void {
+    console.log("O carro está 1km/h mais rápido!");
+  }
+
+  speedDown(): void {
+    console.log("O carro está 1km/h mais devagar!");
+  }
+
+  stop(): void {
+    console.log("O carro foi parado!");
+  }
+
+  turn(direction: Directions): void {
+    console.log(`O carro virou à ${direction}`);
+  }
+}
