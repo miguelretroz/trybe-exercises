91c5d78b260613dcc1c249d592d8ae85338e405d
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-28 00:40:57 -0300

    Exercise 01 Year Months-> Create year months enum

---
+++ typescript/day-02/exercises/year-months/yearMonths.ts
@@ -0,0 +1,16 @@
+enum YearMonths {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+console.log(YearMonths);
