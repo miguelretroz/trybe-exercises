e556680116bbd1a1b1001f784c5423c5d6e97c22
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-30 12:50:24 -0300

    Examples-> array size

--- computer-science/36.2/examples/array_example.py
+++ computer-science/36.2/examples/array_example.py
@@ -1,11 +1,26 @@
 from Array import Array
+import sys
 
 array = Array()
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
 array.set(0, "Felipe")
 array.set(1, "Ana")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
 array.set(2, "Shirley")
 array.set(3, "Miguel")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
 
+array.set(4, "Alberto")
+array.set(5, "Marta")
+array.set(6, "Túlio")
+array.set(7, "Michelle")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
 
 print(array.get(0))
 print(array.get(1))
