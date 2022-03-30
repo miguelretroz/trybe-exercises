45e5600d43609b31676607cb2ffaa0835dfce3aa
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-30 12:25:23 -0300

    Examples-> Create a separete file for the Array class

---
+++ computer-science/36.2/examples/Array.py
@@ -0,0 +1,15 @@
+class Array:
+    def __init__(self):
+        self.data = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def __str__(self):
+        return str(self.data)
+
+    def get(self, index):
+        return self.data[index]
+
+    def set(self, index, value):
+        self.data.insert(index, value)
