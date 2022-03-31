d08dca5983e2da3c4ae6503192192d9c2e94421b
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-03-31 13:53:42 -0300

    Examples-> Create class HashMap

---
+++ computer-science/36.3/examples/HashMap.py
@@ -0,0 +1,18 @@
+class HashMap:
+    def __init__(self):
+        self.buckets = [None for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address] = employee
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        return self._buckets[address].name
+
+    def has(self, id_num):
+        address = self.get_address(id_num)
+        return self._buckets[address] is not None
