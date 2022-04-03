f218f7a282c6ae0f580e7f1f79b736ea456d234b
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-02 21:04:25 -0300

    Exercise 02 Substring-> Implement function `substring`

---
+++ computer-science/36.4/exercises/substring/main.py
@@ -0,0 +1,6 @@
+from substring import substring
+
+if __name__ == "__main__":
+    str = "serdevemuitolegalmasehprecisoestudarbastante"
+    highest_substring = substring(str)
+    print(f"Highest substring of \"{str}\":", highest_substring)
