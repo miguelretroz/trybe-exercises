64742a5448d9c6dd90b8ccc4a35ea432d9ef1f94
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-05 18:11:04 -0300

    Examples-> Create LinkedList class

---
+++ computer-science/37.1/examples/linked_list.py
@@ -0,0 +1,14 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self):
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+
