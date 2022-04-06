c55f1b04a0d048e8a4df0b66a01aa37fcc961cc8
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 05:43:48 -0300

    Exercises-> Implement method `__get_node_at` in `LinkedList` class

--- computer-science/37.1/exercises/linked_list.py
+++ computer-science/37.1/exercises/linked_list.py
@@ -12,6 +12,17 @@ class LinkedList:
     def __len__(self):
         return self.__length
 
+    def __get_node_at(self, position):
+        if position < 1:
+            return self.head_value
+
+        current_value = self.head_value
+        while position >= 1 and current_value.next:
+            current_value = current_value.next
+            position -= 1
+
+        return current_value
+
     def insert_first(self, value):
         first_value = Node(value)
         first_value.next = self.head_value
