3eb68646773e33a2e61c9548f502e8187a3713b6
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 11:34:02 -0300

    Exercises-> Create `Stack` class

---
+++ computer-science/37.2/exercises/stack.py
@@ -0,0 +1,39 @@
+class Stack():
+    def __init__(self):
+        self._data = list()
+
+    def __str__(self):
+        str_items = ""
+        for i in range(self.size()):
+            value = self._data[i]
+            str_items += str(value)
+            if i + 1 < self.size():
+                str_items += ", "
+
+        return f"Stack(\"{str_items}\")"
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        value = self._data[-1]
+        del self._data[-1]
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        value = self._data[-1]
+        return value
+
+    def clear(self):
+        self._data.clear()
