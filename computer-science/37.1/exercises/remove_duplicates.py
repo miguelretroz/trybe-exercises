e41a3d42eb7c178e1ad2acf0644e102d0e828245
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 07:12:01 -0300

    Exercises-> Implement function to remove duplicates of double linked list

--- computer-science/37.1/exercises/remove_duplicates.py
+++ computer-science/37.1/exercises/remove_duplicates.py
@@ -1,11 +1,15 @@
+from linked_list import LinkedList
+from double_linked_list import DoubleLinkedList
+
+
 def remove_duplicates_of_linked_list(linked_list):
     new_linked_list = LinkedList()
-    list_elements = set()
+    elements_without_duplicates = set()
 
     current_list_element = linked_list.remove_first()
     while current_list_element:
-        if current_list_element.value not in list_elements:
-            list_elements.add(current_list_element.value)
+        if current_list_element.value not in elements_without_duplicates:
+            elements_without_duplicates.add(current_list_element.value)
             new_linked_list.insert_first(current_list_element.value)
 
         current_list_element = linked_list.remove_first()
@@ -13,9 +17,27 @@ def remove_duplicates_of_linked_list(linked_list):
     return new_linked_list
 
 
-if __name__ == "__main__":
-    from linked_list import LinkedList
+def remove_duplicates_of_double_linked_list(double_linked_list):
+    new_double_linked_list = DoubleLinkedList()
+    elements_without_duplicates = set()
+    elements_duplicates = set()
 
+    current_list_element = double_linked_list.remove_first()
+    while current_list_element:
+        if current_list_element.value not in elements_without_duplicates:
+            elements_without_duplicates.add(current_list_element.value)
+        else:
+            elements_duplicates.add(current_list_element.value)
+
+        current_list_element = double_linked_list.remove_first()
+
+    for element in elements_without_duplicates.difference(elements_duplicates):
+        new_double_linked_list.insert_last(element)
+
+    return new_double_linked_list
+
+
+if __name__ == "__main__":
     linked_list = LinkedList()
 
     linked_list.insert_first(2)
@@ -24,3 +46,20 @@ if __name__ == "__main__":
     print(linked_list)
 
     print(remove_duplicates_of_linked_list(linked_list))
+
+    double_linked_list = DoubleLinkedList()
+
+    double_linked_list.insert_last(1)
+    double_linked_list.insert_last(1)
+    double_linked_list.insert_last(2)
+    print(remove_duplicates_of_double_linked_list(double_linked_list))
+
+    double_linked_list_two = DoubleLinkedList()
+
+    double_linked_list_two.insert_last(1)
+    double_linked_list_two.insert_last(1)
+    double_linked_list_two.insert_last(2)
+    double_linked_list_two.insert_last(3)
+    double_linked_list_two.insert_last(3)
+
+    print(remove_duplicates_of_double_linked_list(double_linked_list_two))
