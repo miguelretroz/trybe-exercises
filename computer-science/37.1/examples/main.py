3bb99caf906e2d23db5ad1a628750574145229b3
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-06 04:15:44 -0300

    Examples-> Run examples with `DoubleLinkedList`

--- computer-science/37.1/examples/main.py
+++ computer-science/37.1/examples/main.py
@@ -1,4 +1,5 @@
 from linked_list import LinkedList
+from double_linked_list import DoubleLinkedList
 
 if __name__ == "__main__":
     linked_list = LinkedList()
@@ -30,3 +31,34 @@ if __name__ == "__main__":
     linked_list.insert_at(8, 3)
     linked_list.insert_at(9, 4)
     print(linked_list.get_element_at(3))  # saída: Node(value=8 next=None)
+
+    print("--- Double Linked List ---")
+    double_linked_list = DoubleLinkedList()
+
+    print(double_linked_list.is_empty())  # saída: True
+    double_linked_list.insert_first(1)
+    print(double_linked_list)
+
+    double_linked_list.insert_first(2)
+    print(double_linked_list)
+
+    double_linked_list.insert_last(3)
+    print(double_linked_list)
+
+    double_linked_list.remove_last()
+    print(double_linked_list)
+
+    double_linked_list.remove_first()
+    print(double_linked_list)
+
+    double_linked_list.insert_at(5, 1)
+    print(double_linked_list)
+
+    double_linked_list.remove_at(0)
+    print(double_linked_list)
+
+    double_linked_list.insert_at(6, 1)
+    double_linked_list.insert_at(7, 2)
+    double_linked_list.insert_at(8, 3)
+    double_linked_list.insert_at(9, 4)
+    print(double_linked_list.get_element_at(3))
