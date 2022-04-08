58617662b3bdea4cae9dd84c439c46472653e29c
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-08 10:58:13 -0300

    Examples-> Implement books routes

--- typescript/day-03/examples/index.ts
+++ typescript/day-03/examples/index.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express';
+import BooksRoutes from './routes/books';
+
 const app = express();
+app.use(express.json());
 
 const PORT = 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript');
 });
+
+app.use(BooksRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
