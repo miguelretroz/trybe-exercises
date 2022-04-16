ec23ea38e44c74980624827ea3ec1b36e54c4d57
Miguel Retroz <contato.miguelretroz@gmail.com>
2022-04-16 20:29:30 -0300

    Exercise 01 People-> Create users model `register`

--- typescript/day-03/exercises/people/models/users.ts
+++ typescript/day-03/exercises/people/models/users.ts
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import { v4 as uuidv4 } from 'uuid';
 
 import User from '../interfaces/User';
 
@@ -12,6 +13,22 @@ export const getByEmail = async (userEmail: string): Promise<User | null> => {
   return null;
 };
 
+export const register = async (userData: User): Promise<User> => {
+  const id = uuidv4();
+  userData.id = id;
+
+  const users: User[] = JSON.parse(await fs.readFile('./users.json', 'utf-8'));
+
+  users.push(userData);
+
+  await fs.writeFile('./users.json', JSON.stringify(users), 'utf-8');
+
+  const { password, ...userDataWithoutPassword } = userData;
+
+  return userDataWithoutPassword;
+};
+
 export default {
+  register,
   getByEmail,
 };
